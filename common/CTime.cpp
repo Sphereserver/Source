4c512b51d84ef7d3284e5ca9fce514290ae6a6c2
e11ess4r <e11ess4r@torfo.org>
2007-02-06 18:57:15 +0000

    CTime.cpp: Removed support for timeval.tv_usec since Linux always returns 0 here. Only seconds are now used.

--- common/CTime.cpp
+++ common/CTime.cpp
@@ -14,11 +14,7 @@ LONGLONG GetTickCount()
 {
 	struct timeval tv;
 	gettimeofday( &tv, NULL );
-	//return (LONGLONG) (((LONGLONG) tv.tv_sec * 1000) + ((LONGLONG) tv.tv_usec/1000));
-	double dtime = tv.tv_sec * 1000000.0;
-	dtime += tv.tv_usec;
-	dtime /= 1000.0;
-	return (LONGLONG) dtime;
+	return ((LONGLONG) ( tv.tv_sec * 1000 ));
 }
 #endif
 
