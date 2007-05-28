76627fda51bfc6656f096fc03ba839688b1fc757
e11ess4r <e11ess4r@torfo.org>
2007-05-28 16:18:44 +0000

    Missing ";" prevented mutex.cpp to be successfully compiled under Linux.

--- sphere/mutex.cpp
+++ sphere/mutex.cpp
@@ -8,7 +8,7 @@ SimpleMutex::SimpleMutex()
 	InitializeCriticalSectionAndSpinCount(&m_criticalSection, 0x80000100);
 #else
 	pthread_mutexattr_settype(&m_criticalSectionAttr, PTHREAD_MUTEX_RECURSIVE_NP);
-	pthread_mutex_init(&m_criticalSection, &m_criticalSectionAttr)
+	pthread_mutex_init(&m_criticalSection, &m_criticalSectionAttr);
 #endif
 }
 
