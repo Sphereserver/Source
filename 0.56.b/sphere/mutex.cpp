e8f468537198ac8daf5f5b01432049e43b0ec6c8
furio <furio@torfo.org>
2007-05-28 00:57:25 +0000

    Simple mutex/lock

---
+++ 0.56.b/sphere/mutex.cpp
@@ -0,0 +1,66 @@
+#define _WIN32_DCOM
+
+#include "mutex.h"
+
+SimpleMutex::SimpleMutex()
+{
+#ifdef _WIN32
+	InitializeCriticalSection(&m_criticalSection);
+#else
+	pthread_mutexattr_settype(&m_criticalSectionAttr, PTHREAD_MUTEX_RECURSIVE_NP);
+	pthread_mutex_init(&m_criticalSection, &m_criticalSectionAttr)
+#endif
+}
+
+SimpleMutex::~SimpleMutex()
+{
+#ifdef _WIN32
+	DeleteCriticalSection(&m_criticalSection);
+#else
+	pthread_mutexattr_destroy(&m_criticalSectionAttr);
+	pthread_mutex_destroy(&m_criticalSection);
+#endif
+}
+
+//lock
+void SimpleMutex::lock()
+{
+#ifdef _WIN32
+	EnterCriticalSection(&m_criticalSection);
+#else
+	pthread_mutex_lock(&m_criticalSection);
+#endif
+}
+
+//unlock
+void SimpleMutex::unlock()
+{
+#ifdef _WIN32
+	LeaveCriticalSection(&m_criticalSection);
+#else
+	pthread_mutex_unlock (&m_criticalSection);
+#endif
+}
+
+SimpleThreadLock::SimpleThreadLock(SimpleMutex &mutex) : m_mutex(mutex), m_locked(true)
+{
+	mutex.lock();
+}
+
+//the destructor
+SimpleThreadLock::~SimpleThreadLock()
+{
+	m_mutex.unlock();
+}
+
+//report the state of locking when used as a boolean
+SimpleThreadLock::operator bool() const
+{
+	return m_locked;
+}
+
+//unlock
+void SimpleThreadLock::setUnlock()
+{
+	m_locked = false;        
+}
