3d66fc2155d19a3ea3c4fffa1d6b440210380225
furio <furio@torfo.org>
2007-07-19 17:56:42 +0000

    SpSpyUO ModKR now intercept send/receive packets. (forgot some files)

---
+++ docs/uokr/spyuo-modkr/Dll/Packets/EncryptionStart.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpyUO.Packets
+{
+    [PacketInfo(0xFF)]
+    public class EncryptionStart : Packet
+    {
+        private byte[] m_Ffs;
+
+        [PacketProp(0)]
+        public uint CryptHeader { get { return (uint)((m_Ffs[2] * 0x1000000) + (m_Ffs[1] * 0x10000) + (m_Ffs[0] * 0x100) + base.Cmd); } }
+
+        public EncryptionStart(PacketReader reader, bool send) : base(reader, send)
+		{
+            m_Ffs = new byte[3];
+            m_Ffs = reader.ReadBytes(3);
+		}
+    }
+}
