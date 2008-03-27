09f04f93ea3cec48918be3bb48e2e0aff85eb64f
furio <furio@torfo.org>
2008-03-27 20:40:11 +0000

    Updated libev to latest version

--- common/libev/ev_select.c
+++ common/libev/ev_select.c
@@ -68,7 +68,8 @@
 
 #include <string.h>
 
-static void select_modify (EV_P_ int fd, int oev, int nev)
+static void
+select_modify (EV_P_ int fd, int oev, int nev)
 {
   if (oev == nev)
     return;
@@ -94,8 +95,8 @@ static void select_modify (EV_P_ int fd, int oev, int nev)
 
 #else
 
-    int word = fd / NFDBITS;
-    int mask = 1UL << (fd % NFDBITS);
+    int     word = fd / NFDBITS;
+    fd_mask mask = 1UL << (fd % NFDBITS);
 
     if (expect_false (vec_max < word + 1))
       {
@@ -107,8 +108,8 @@ static void select_modify (EV_P_ int fd, int oev, int nev)
         vec_wo = ev_realloc (vec_wo, new_max * NFDBYTES); /* could free/malloc */
 
         for (; vec_max < new_max; ++vec_max)
-          ((fd_mask *)vec_ri)[vec_max] =
-          ((fd_mask *)vec_wi)[vec_max] = 0;
+          ((fd_mask *)vec_ri) [vec_max] =
+          ((fd_mask *)vec_wi) [vec_max] = 0;
       }
 
     ((fd_mask *)vec_ri) [word] |= mask;
@@ -239,3 +240,5 @@ select_destroy (EV_P)
   ev_free (vec_wi);
   ev_free (vec_wo);
 }
+
+
