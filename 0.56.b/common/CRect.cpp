035b90dfea4cd2d75582c19beed19f2c83ff3699
shaklaban <shaklaban@torfo.org>
2013-02-12 22:40:11 +0000

    -Fixed #2134:COMPONENTS of the point not working, if MAP not 0.

--- 0.56.b/common/CRect.cpp
+++ 0.56.b/common/CRect.cpp
@@ -389,7 +389,7 @@ bool CPointBase::r_WriteVal( LPCTSTR pszKey, CGString & sVal ) const
 					if (pMultiItem->m_visible == 0)
 						continue;
 
-					CPointMap ptTest(ptMulti.m_x + pMultiItem->m_dx, ptMulti.m_y + pMultiItem->m_dy, ptMulti.m_z + pMultiItem->m_dz);
+					CPointMap ptTest(ptMulti.m_x + pMultiItem->m_dx, ptMulti.m_y + pMultiItem->m_dy, ptMulti.m_z + pMultiItem->m_dz, this->m_map);
 					if (GetDist(ptTest) > 0)
 						continue;
 
