92dee360c62bb9a2c6a2204e26c77f037e6851e7
Roberto García <roberpot@gmail.com>
2015-10-26 12:30:13 +0100

    Add some doxygen doc to CFile.h.
    Move some definitions to CFile.cpp.

--- src/common/CFile.cpp
+++ src/common/CFile.cpp
@@ -4,6 +4,7 @@
 #include <errno.h>	// errno
 #endif
 
+
 bool CFile::SetFilePath( LPCTSTR pszName )
 {
 	ADDTOCALLSTACK("CFile::SetFilePath");
@@ -29,6 +30,131 @@ LPCTSTR CFile::GetFileTitle() const
 	ADDTOCALLSTACK("CFile::GetFileTitle");
 	return( CGFile::GetFilesTitle( GetFilePath()));
 }
+
+bool CFile::Open( LPCTSTR pszName, UINT uMode, CFileException * e )
+{
+	UNREFERENCED_PARAMETER(e);
+	ASSERT( m_hFile == NOFILE_HANDLE );
+	SetFilePath( pszName );
+
+#ifdef _WIN32
+	DWORD dwShareMode, dwCreationDisposition, dwDesiredAccess;
+
+	dwDesiredAccess = GENERIC_READ;
+	if ( uMode & OF_WRITE )
+		dwDesiredAccess |= GENERIC_WRITE;
+	if ( uMode & OF_READWRITE )
+		dwDesiredAccess |= (GENERIC_READ | GENERIC_WRITE);
+
+	if ( uMode & OF_SHARE_COMPAT )
+		dwShareMode = (FILE_SHARE_READ | FILE_SHARE_WRITE);
+	else if ( uMode & OF_SHARE_EXCLUSIVE )
+		dwShareMode = 0;
+	else if ( uMode & OF_SHARE_DENY_WRITE )
+		dwShareMode = FILE_SHARE_READ;
+	else if ( uMode & OF_SHARE_DENY_READ )
+		dwShareMode = FILE_SHARE_WRITE;
+	else if ( uMode & OF_SHARE_DENY_NONE )
+		dwShareMode = (FILE_SHARE_READ | FILE_SHARE_WRITE);
+	else
+		dwShareMode = 0;
+
+	if ( uMode & OF_CREATE )
+		dwCreationDisposition = (OPEN_ALWAYS|CREATE_NEW);
+	else
+		dwCreationDisposition = OPEN_EXISTING;
+
+	m_hFile = CreateFile( GetFilePath(), dwDesiredAccess, dwShareMode, NULL, dwCreationDisposition, FILE_ATTRIBUTE_NORMAL, NULL );
+#else // _WIN32
+	m_hFile = open( GetFilePath(), uMode );
+#endif // _WIN32
+	return( m_hFile != NOFILE_HANDLE );
+}
+
+void CFile::Close()
+{
+	if ( m_hFile != NOFILE_HANDLE )
+	{
+#ifdef _WIN32
+		CloseHandle( m_hFile );
+#else
+		close( m_hFile );
+#endif
+		m_hFile = NOFILE_HANDLE;
+	}
+}
+
+DWORD CFile::GetLength()
+{
+	// Get the size of the file.
+	DWORD lPos = GetPosition();	// save current pos.
+	DWORD lSize = SeekToEnd();
+	Seek( lPos, SEEK_SET );	// restore previous pos.
+	return( lSize );
+}
+
+DWORD CFile::GetPosition() const
+{
+#ifdef _WIN32
+	return SetFilePointer( m_hFile, 0, NULL, FILE_CURRENT );
+#else
+	return( lseek( m_hFile, 0, SEEK_CUR ) );
+#endif
+}
+
+DWORD CFile::Seek( LONG lOffset, UINT iOrigin )
+{
+#ifdef _WIN32
+	return SetFilePointer( m_hFile, lOffset, NULL, iOrigin );
+#else
+	if ( m_hFile <= 0 )
+		return( -1 );
+	return( lseek( m_hFile, lOffset, iOrigin ));
+#endif
+}
+
+DWORD CFile::Read( void * pData, DWORD dwLength ) const
+{
+#ifdef _WIN32
+	DWORD dwRead;
+	if ( ! ReadFile( m_hFile, pData, dwLength, &dwRead, NULL ))
+	{
+		NotifyIOError("read");
+		return 0;
+	}
+	return dwRead;
+#else
+	return( read( m_hFile, pData, (long) dwLength ));
+#endif
+}
+
+bool CFile::Write( const void * pData, DWORD dwLength ) const
+{
+#ifdef _WIN32
+	DWORD dwWritten;
+	BOOL ret = ::WriteFile( m_hFile, pData, dwLength, &dwWritten, NULL );
+	if ( ret == FALSE )
+	{
+		NotifyIOError("write");
+		return false;
+	}
+	return true;
+#else
+	return( write( m_hFile, (const char *) pData, (long) dwLength ) == (long) dwLength );
+#endif
+}
+
+#ifdef _WIN32
+void CFile::NotifyIOError( LPCTSTR szMessage )
+{
+	LPVOID lpMsgBuf;
+	FormatMessage( FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, GetLastError(), 0, reinterpret_cast<LPTSTR>(&lpMsgBuf), 0, NULL );
+	DEBUG_ERR(( "File I/O \"%s\" failed on file \"%s\" (%ld): %s\n", szMessage, static_cast<LPCTSTR>(GetFilePath()), GetLastError(), static_cast<LPTSTR>(lpMsgBuf) ));
+
+	LocalFree( lpMsgBuf );
+}
+#endif
+
 
 //***************************************************************************
 // -CGFile
@@ -215,4 +341,112 @@ bool CFileText::OpenBase( void FAR * pszExtra )
 	m_hFile = (OSFILE_TYPE)STDFUNC_FILENO(m_pStream);
 
 	return( true );
+}
+
+DWORD CFileText::Seek( LONG offset, UINT origin )
+{
+	// true = success
+	if ( ! IsFileOpen())
+		return 0;
+	if ( offset < 0 )
+		return 0;
+	if ( fseek( m_pStream, offset, origin ) != 0 )
+		return 0;
+	long position = ftell(m_pStream);
+	if (position < 0)
+		return 0;
+	return position;
+}
+
+void CFileText::Flush() const
+{
+	if ( ! IsFileOpen())
+		return;
+	ASSERT(m_pStream);
+	fflush(m_pStream);
+}
+
+DWORD CFileText::GetPosition() const
+{
+	// RETURN: -1 = error.
+	if ( ! IsFileOpen())
+		return static_cast<DWORD>(-1);
+	return( ftell(m_pStream));
+}
+
+DWORD CFileText::Read( void * pBuffer, size_t sizemax ) const
+{
+	// This can return: EOF(-1) constant.
+	// returns the number of full items actually read
+	ASSERT(pBuffer);
+	if ( IsEOF())
+		return( 0 );	// LINUX will ASSERT if we read past end.
+	return( static_cast<DWORD>(fread( pBuffer, 1, sizemax, m_pStream )));
+}
+
+TCHAR * CFileText::ReadString( TCHAR * pBuffer, size_t sizemax ) const
+{
+	// Read a line of text. NULL = EOF
+	ASSERT(pBuffer);
+	if ( IsEOF())
+		return( NULL );	// LINUX will ASSERT if we read past end.
+	return( fgets( pBuffer, static_cast<int>(sizemax), m_pStream ));
+}
+
+#ifndef _WIN32
+	bool CFileText::Write( const void * pData, DWORD iLen ) const
+#else
+	bool CFileText::Write( const void * pData, DWORD iLen )
+#endif
+{
+	// RETURN: 1 = success else fail.
+	ASSERT(pData);
+	if ( ! IsFileOpen())
+		return( false );
+#ifdef _WIN32 //	Windows flushing, the only safe mode to cancel it ;)
+	if ( !bNoBuffer )
+	{
+		setvbuf(m_pStream, NULL, _IONBF, 0);
+		bNoBuffer = true;
+	}
+#endif
+	size_t iStatus = fwrite( pData, iLen, 1, m_pStream );
+#ifndef _WIN32	// However, in unix, it works
+	fflush( m_pStream );
+#endif
+	return ( iStatus == 1 );
+}
+
+bool CFileText::WriteString( LPCTSTR pStr )
+{
+	// RETURN: < 0 = failed.
+	ASSERT(pStr);
+	return( Write( pStr, static_cast<DWORD>(strlen( pStr ))));
+}
+
+bool CFileText::IsEOF() const
+{
+	if ( ! IsFileOpen())
+		return( true );
+	return(( feof( m_pStream )) ? true : false );
+}
+
+size_t CFileText::VPrintf( LPCTSTR pFormat, va_list args )
+{
+	ASSERT(pFormat);
+	if ( ! IsFileOpen())
+		return static_cast<size_t>(-1);
+
+	size_t lenret = vfprintf( m_pStream, pFormat, args );
+	return( lenret );
+}
+
+size_t _cdecl CFileText::Printf( LPCTSTR pFormat, ... )
+{
+	ASSERT(pFormat);
+	va_list vargs;
+	va_start( vargs, pFormat );
+	size_t iRet = VPrintf( pFormat, vargs );
+	va_end( vargs );
+	return( iRet );
 }
--- src/common/CFile.h
+++ src/common/CFile.h
@@ -1,6 +1,10 @@
+/**
+* @file CFile.h
+*/
+
+#pragma once
 #ifndef _INC_CFILE_H
 #define _INC_CFILE_H
-#pragma once
 
 #ifndef _WIN32
 #ifdef LONG
@@ -49,249 +53,243 @@
 class CGrayError;
 #define CFileException CGrayError
 
-// Dupe the MFC functionality we need
+/**
+* @brief Class that dupes the MFC functionality we need
+*/
 class CFile
 {
 public:
 	static const char *m_sClassName;
-	OSFILE_TYPE m_hFile;
+	OSFILE_TYPE m_hFile;	///< File type.
 
 protected:
-	CGString m_strFileName;
+	CGString m_strFileName;	///< File name (with path).
 
 public:
-	// File Name and path.
-	const CGString & GetFilePath() const	// for compatibility with MFC
-	{
-		return( m_strFileName);
-	}
+	/**
+	* @brief Get file name and path (for compatibility with MFC)
+	* @return file name and path.
+	*/
+	const CGString & GetFilePath() const { return( m_strFileName); }
+	/**
+	* @brief Sets a new file path.
+	*
+	* If CFile has already have a file path, close it if is opened and open new 
+	* file.
+	* @param pszName new file path.
+	* @return true if new file name is setted, false otherwise.
+	*/
 	virtual bool SetFilePath( LPCTSTR pszName );
-
+	/**
+	* @brief Gets the basename of the file.
+	* @return the basename of the file (name withouth paths).
+	*/
 	LPCTSTR GetFileTitle() const;
-
-	// File Open/Close 
-	virtual bool Open( LPCTSTR pszName = NULL, UINT uMode = OF_READ | OF_SHARE_DENY_NONE, CFileException * e = NULL )
-	{
-		UNREFERENCED_PARAMETER(e);
-		ASSERT( m_hFile == NOFILE_HANDLE );
-		SetFilePath( pszName );
-
+	/**
+	* @brief Open a file in a specified mode.
+	* @param pszName file to open.
+	* @param uMode open mode.
+	* @param e TODOC.
+	* @return true if file is open, false otherwise.
+	*/
+	virtual bool Open( LPCTSTR pszName = NULL, UINT uMode = OF_READ | OF_SHARE_DENY_NONE, CFileException * e = NULL );
+	/**
+	* @brief Closes the file if is open.
+	*/
+	virtual void Close();
+	/**
+	* @brief Sets the position indicator at the begin of the file.
+	*/
+	void SeekToBegin() { Seek( 0, SEEK_SET ); }
+	/**
+	* @brief Sets the position indicator at the end of the file.
+	* @return The length of the file on success, -1 on error.
+	*/
+	DWORD SeekToEnd() { return( Seek( 0, SEEK_END )); }
+	/**
+	* @brief Get the length of the file.
+	* @return the length of the file.
+	*/
+	DWORD GetLength();
+	/**
+	* @brief Gets the position indicator of the file.
+	* @return The position indicator of the file.
+	*/
+	virtual DWORD GetPosition() const;
+	/**
+	* @brief Set the position indicator.
+	* @param lOffset position to set.
+	* @param iOrigin origin (current position or init of the file).
+	* @return position where the position indicator is set on success, -1 on error.
+	*/
+	virtual DWORD Seek( LONG lOffset = 0, UINT iOrigin = SEEK_SET );
+	/**
+	* @brief Reads data from the file.
+	* @param pData buffer where store the readed data.
+	* @param dwLength count of bytes to read.
+	* @return count of bytes readed.
+	*/
+	virtual DWORD Read( void * pData, DWORD dwLength ) const;
+	/**
+	* @brief writes supplied data into file.
+	* @param pData data to write.
+	* @param dwLength lenght of the data to write.
+	* @return true is success, false otherwise.
+	*/
+	virtual bool Write( const void * pData, DWORD dwLength ) const;
 #ifdef _WIN32
-		DWORD dwShareMode, dwCreationDisposition, dwDesiredAccess;
-
-		dwDesiredAccess = GENERIC_READ;
-		if ( uMode & OF_WRITE )
-			dwDesiredAccess |= GENERIC_WRITE;
-		if ( uMode & OF_READWRITE )
-			dwDesiredAccess |= (GENERIC_READ | GENERIC_WRITE);
-
-
-		if ( uMode & OF_SHARE_COMPAT )
-			dwShareMode = (FILE_SHARE_READ | FILE_SHARE_WRITE);
-		else if ( uMode & OF_SHARE_EXCLUSIVE )
-			dwShareMode = 0;
-		else if ( uMode & OF_SHARE_DENY_WRITE )
-			dwShareMode = FILE_SHARE_READ;
-		else if ( uMode & OF_SHARE_DENY_READ )
-			dwShareMode = FILE_SHARE_WRITE;
-		else if ( uMode & OF_SHARE_DENY_NONE )
-			dwShareMode = (FILE_SHARE_READ | FILE_SHARE_WRITE);
-		else
-			dwShareMode = 0;
-
-
-		if ( uMode & OF_CREATE )
-			dwCreationDisposition = (OPEN_ALWAYS|CREATE_NEW);
-		else
-			dwCreationDisposition = OPEN_EXISTING;
-
-		m_hFile = CreateFile( GetFilePath(), dwDesiredAccess, dwShareMode, NULL, dwCreationDisposition, FILE_ATTRIBUTE_NORMAL, NULL );
-#else
-		m_hFile = open( GetFilePath(), uMode );
-#endif
-		return( m_hFile != NOFILE_HANDLE );
-	}
-
-	virtual void Close()
-	{
-		if ( m_hFile != NOFILE_HANDLE )
-		{
-#ifdef _WIN32
-			CloseHandle( m_hFile );
-#else
-			close( m_hFile );
-#endif
-			m_hFile = NOFILE_HANDLE;
-		}
-	}
-
-	// File Access
-	void SeekToBegin()
-	{
-		Seek( 0, SEEK_SET );
-	}
-	DWORD SeekToEnd()
-	{
-		return( Seek( 0, SEEK_END ));
-	}
-	DWORD GetLength()
-	{
-		// Get the size of the file.
-		DWORD lPos = GetPosition();	// save current pos.
-		DWORD lSize = SeekToEnd();
-		Seek( lPos, SEEK_SET );	// restore previous pos.
-		return( lSize );
-	}
-
-#ifdef _WIN32
-	virtual DWORD GetPosition() const
-	{
-		return SetFilePointer( m_hFile, 0, NULL, FILE_CURRENT );
-	}
-
-	virtual DWORD Seek( LONG lOffset = 0, UINT iOrigin = SEEK_SET )
-	{
-		// true = success.
-		return SetFilePointer( m_hFile, lOffset, NULL, iOrigin );
-	}
-
-	virtual DWORD Read( void * pData, DWORD dwLength ) const
-	{
-		// RETURN: length of the read data.
-		DWORD dwRead;
-		if ( ! ReadFile( m_hFile, pData, dwLength, &dwRead, NULL ))
-		{
-			NotifyIOError("read");
-			return 0;
-		}
-		return dwRead;
-	}
-
-	virtual bool Write( const void * pData, DWORD dwLength ) const
-	{
-		DWORD dwWritten;
-		BOOL ret = ::WriteFile( m_hFile, pData, dwLength, &dwWritten, NULL );
-		if ( ret == FALSE )
-		{
-			NotifyIOError("write");
-			return false;
-		}
-		return( ret != FALSE );
-	}
-
-	void NotifyIOError( LPCTSTR szMessage ) const
-	{
-		LPVOID lpMsgBuf;
-		FormatMessage( FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, GetLastError(), 0, reinterpret_cast<LPTSTR>(&lpMsgBuf), 0, NULL );
-		DEBUG_ERR(( "File I/O \"%s\" failed on file \"%s\" (%ld): %s\n", szMessage, static_cast<LPCTSTR>(GetFilePath()), GetLastError(), static_cast<LPTSTR>(lpMsgBuf) ));
-
-		LocalFree( lpMsgBuf );
-	}
-#else
-	virtual DWORD GetPosition() const
-	{
-		return( lseek( m_hFile, 0, SEEK_CUR ) );
-	}
-	virtual DWORD Seek( LONG lOffset = 0, UINT iOrigin = SEEK_SET )
-	{
-		// true = success.
-		if ( m_hFile <= 0 )
-			return( -1 );
-		return( lseek( m_hFile, lOffset, iOrigin ));
-	}
-	virtual DWORD Read( void * pData, DWORD dwLength ) const
-	{
-		// RETURN: length of the read data.
-		return( read( m_hFile, pData, (long) dwLength ));
-	}
-	virtual bool Write( const void * pData, DWORD dwLength ) const
-	{
-		return( write( m_hFile, (const char *) pData, (long) dwLength ) == (long) dwLength );
-	}
+	/**
+	* @brief Notify a file input / output error (win32 only).
+	* @param szMessage error to notify.
+	*/
+	void NotifyIOError( LPCTSTR szMessage ) const;
 #endif
 
 public:
-	CFile()
-	{
-		m_hFile = NOFILE_HANDLE;
-	}
-	virtual ~CFile()
-	{
-		Close();
-	}
-
+	CFile() { m_hFile = NOFILE_HANDLE; }
+	virtual ~CFile() { Close(); }
 private:
+	/**
+	* @brief No copy on construction allowed.
+	*/
 	CFile(const CFile& copy);
+	/**
+	* @brief No copy allowed.
+	*/
 	CFile& operator=(const CFile& other);
 };
 
-class CGFile : public CFile	// try to be compatible with MFC CFile class.
+
+/**
+* Try to be compatible with MFC CFile class.
+*/
+class CGFile : public CFile
 {
 private:
-	UINT m_uMode;	// MMSYSTEM may use 32 bit flags.
-private:
-	virtual bool OpenBase( void * pExtra );
-	virtual void CloseBase();
-
+	UINT m_uMode;	///< MMSYSTEM may use 32 bit flags.
 public:
 	static const char *m_sClassName;
-	// File Name and Path.
-
+private:
+	/**
+	* @brief Open file with CFile method.
+	* @param pExtra unused.
+	* @return true if file is open, false otherwise.
+	*/
+	virtual bool OpenBase( void * pExtra );
+	/**	
+	* @brief Closes the file if is open.
+	*/
+	virtual void CloseBase();
+public:
+	/**
+	* @brief Gets the basename of the file.
+	* @param pszPath file path.
+	* @return the basename of the file (name withouth paths).
+	*/
 	static LPCTSTR GetFilesTitle( LPCTSTR pszPath );
+	/**
+	* @brief Gets the file extension, if any.
+	* @param pszName file path where get the extension.
+	* @return The extension of the file or NULL if the file has no extension.
+	*/
 	static LPCTSTR GetFilesExt( LPCTSTR pszName );
+	/**
+	* @brief Merges path and filename, adding slashes if needed.
+	* @param pszBase path.
+	* @param pszName filename.
+	* @return merged path.
+	*/
 	static CGString GetMergedFileName( LPCTSTR pszBase, LPCTSTR pszName );
-
+	/**
+	* @brief Return the last IO error code.
+	* @return IO error code.
+	*/
 	static int GetLastError();
-
+	/**
+	* @brief Gets the file extension, if any.
+	* @return The extension of the file or NULL if the file has no extension.
+	*/
 	LPCTSTR GetFileExt() const;
-
-	// File Mode stuff.
+	/**
+	* @brief Check if file es in binary mode.
+	* @return Always true (this method is virtual).
+	*/
 	virtual bool IsBinaryMode() const { return true; }
-	UINT GetMode() const
-	{
-		return( m_uMode & 0x0FFFFFFF );	// get rid of OF_NONCRIT type flags
-	}
-	UINT GetFullMode() const {
-		return m_uMode;
-	}
-	bool IsWriteMode() const
-	{
-		return(( m_uMode & OF_WRITE ) ? true : false );
-	}
+	/**
+	 * @brief Get open mode (OF_NONCRIT, OF_TEXT, OF_BINARY, OF_DEFAULTMODE)
+	 *
+	 * Get rid of OF_NONCRIT type flags
+	 * @return mode flags.
+	*/
+	UINT GetMode() const { return( m_uMode & 0x0FFFFFFF ); }
+	/**
+	* @brief Get open mode (full).
+	* @return full mode flags.
+	*/
+	UINT GetFullMode() const { return m_uMode; }
+	/**
+	* @brief Check if file is open for write.
+	* @return true if file is open for write, false otherwise.
+	*/
+	bool IsWriteMode() const { return ( m_uMode & OF_WRITE ); }
 
-	// File Open/Close 
-	virtual bool IsFileOpen() const
-	{
-		return ( m_hFile != NOFILE_HANDLE );
-	}
+	/**
+	* @brief Check if file is open.
+	* @return true if file is open, false otherwise.
+	*/
+	virtual bool IsFileOpen() const { return ( m_hFile != NOFILE_HANDLE ); }
+	/**
+	* @brief Open a file in a specified mode.
+	* @param pszName file to open.
+	* @param uMode open mode.
+	* @param pExtra TODOC.
+	* @return true if file is open, false otherwise.
+	*/
 	virtual bool Open( LPCTSTR pszName = NULL, UINT uMode = OF_READ | OF_SHARE_DENY_NONE, void * pExtra = NULL );
+	/**
+	* @brief Closes the file if is open.
+	*/
 	virtual void Close();
 	
 	// File Access
 public:
-	CGFile()
-	{
-		m_uMode = 0;
-	}
-	virtual ~CGFile()
-	{
-		Close();
-	}
+	CGFile() { m_uMode = 0; }
+	virtual ~CGFile() { Close(); }
 
 private:
+	/**
+	* @brief No copy on construction allowed.
+	*/
 	CGFile(const CGFile& copy);
+	/**
+	* @brief No copy allowed.
+	*/
 	CGFile& operator=(const CGFile& other);
 };
 
-class CFileText : public CGFile	// Try to be compatible with MFC CStdioFile
+
+/**
+* @brief Text files. Try to be compatible with MFC CFile class.
+*/
+class CFileText : public CGFile
 {
 public:
 	static const char *m_sClassName;
-	FILE * m_pStream;		// the current open script type file.
+	FILE * m_pStream;		///< The current open script type file.
 protected:
+	/**
+	* @brief Get open mode in string format.
+	*
+	* Formats:
+	* - "rb"
+	* - "w"
+	* - "wb"
+	* - "a+b"
+	* @return string that describes the open mode.
+	*/
 	LPCTSTR GetModeStr() const;
 #ifdef _WIN32
-	bool	bNoBuffer;
+	bool	bNoBuffer;	///< TODOC.
 #endif
 protected:
 	virtual bool OpenBase( void * pExtra );
@@ -304,117 +302,92 @@ public:
 		bNoBuffer = false;
 #endif
 	}
-	virtual ~CFileText()
-	{
-		Close();
-	}
+	virtual ~CFileText() { Close(); }
 private:
+	/**
+	* @brief No copy on construction allowed.
+	*/
 	CFileText(const CFileText& copy);
+	/**
+	* @brief No copy allowed.
+	*/
 	CFileText& operator=(const CFileText& other);
 public:
-	virtual DWORD Seek( LONG offset = 0, UINT origin = SEEK_SET )
-	{
-		// true = success
-		if ( ! IsFileOpen())
-			return 0;
-		if ( offset < 0 )
-			return 0;
-		if ( fseek( m_pStream, offset, origin ) != 0 )
-			return 0;
-		long position = ftell(m_pStream);
-		if (position < 0)
-			return 0;
-		return position;
-	}
-	void Flush() const
-	{
-		if ( ! IsFileOpen())
-			return;
-		ASSERT(m_pStream);
-		fflush(m_pStream);
-	}
-	DWORD GetPosition() const
-	{
-		// RETURN: -1 = error.
-		if ( ! IsFileOpen())
-			return static_cast<DWORD>(-1);
-		return( ftell(m_pStream));
-	}
-	DWORD Read( void * pBuffer, size_t sizemax ) const
-	{
-		// This can return: EOF(-1) constant.
-		// returns the number of full items actually read
-		ASSERT(pBuffer);
-		if ( IsEOF())
-			return( 0 );	// LINUX will ASSERT if we read past end.
-		return( static_cast<DWORD>(fread( pBuffer, 1, sizemax, m_pStream )));
-	}
-	TCHAR * ReadString( TCHAR * pBuffer, size_t sizemax ) const
-	{
-		// Read a line of text. NULL = EOF
-		ASSERT(pBuffer);
-		if ( IsEOF())
-			return( NULL );	// LINUX will ASSERT if we read past end.
-		return( fgets( pBuffer, static_cast<int>(sizemax), m_pStream ));
-	}
-	bool Write( const void * pData, DWORD iLen )
+	/**
+	* @brief Set the position indicator.
+	* @param offset position to set.
+	* @param origin origin (current position or init of the file).
+	* @return position where the position indicator is set on success, 0 on error.
+	*/
+	virtual DWORD Seek( LONG offset = 0, UINT origin = SEEK_SET );
+	/**
+	* @brief Write changes to disk.
+	*/
+	void Flush() const;
+	/**
+	* @brief Get position indicator position.
+	* @return The position indicator if file is opened, -1 otherwise.
+	*/
+	DWORD GetPosition() const;
+	/**
+	* @brief Reads data from the file.
+	* @param pBuffer buffer where store the readed data.
+	* @param sizemax count of bytes to read.
+	* @return count of bytes readed.
+	*/
+	DWORD Read( void * pBuffer, size_t sizemax ) const;
+	/**
+	* @brief Reads from a file a line (up to sizemax - 1 characters).
+	* @param pBuffer buffer where store the readed data.
+	* @param sizemax count of characters to read.
+	* @return the str readed if success, NULL on errors.
+	*/
+	TCHAR * ReadString( TCHAR * pBuffer, size_t sizemax ) const;
+	/**
+	* @brief writes supplied data into file.
+	* @param pData data to write.
+	* @param iLen lenght of the data to write.
+	* @return true is success, false otherwise.
+	*/
 #ifndef _WIN32
-		const
+	bool Write( const void * pData, DWORD iLen ) const;
+#else
+	bool Write( const void * pData, DWORD iLen );
 #endif
-	{
-		// RETURN: 1 = success else fail.
-		ASSERT(pData);
-		if ( ! IsFileOpen())
-			return( false );
-#ifdef _WIN32 //	Windows flushing, the only safe mode to cancel it ;)
-		if ( !bNoBuffer )
-		{
-			setvbuf(m_pStream, NULL, _IONBF, 0);
-			bNoBuffer = true;
-		}
-#endif
-		size_t iStatus = fwrite( pData, iLen, 1, m_pStream );
-#ifndef _WIN32	// However, in unix, it works
-		fflush( m_pStream );
-#endif
-		return ( iStatus == 1 );
-	}
-	bool WriteString( LPCTSTR pStr )
-	{
-		// RETURN: < 0 = failed.
-		ASSERT(pStr);
-		return( Write( pStr, static_cast<DWORD>(strlen( pStr ))));
-	}
-
-	bool IsFileOpen() const
-	{
-		return( m_pStream != NULL );
-	}
+	/**
+	* @brief write string into file.
+	* @return true is success, false otherwise.
+	*/
+	bool WriteString( LPCTSTR pStr );
+	/**
+	* @brief Check if file is open.
+	* @return true if is open, false otherwise.
+	*/
+	bool IsFileOpen() const { return( m_pStream != NULL ); }
+	/**
+	* @brief Check if file is open in binary mode.
+	* @return false always.
+	*/
 	bool IsBinaryMode() const { return false; }
-	bool IsEOF() const
-	{
-		if ( ! IsFileOpen())
-			return( true );
-		return(( feof( m_pStream )) ? true : false );
-	}
-	size_t VPrintf( LPCTSTR pFormat, va_list args )
-	{
-		ASSERT(pFormat);
-		if ( ! IsFileOpen())
-			return static_cast<size_t>(-1);
-
-		size_t lenret = vfprintf( m_pStream, pFormat, args );
-		return( lenret );
-	}
-	size_t _cdecl Printf( LPCTSTR pFormat, ... ) __printfargs(2,3)
-	{
-		ASSERT(pFormat);
-		va_list vargs;
-		va_start( vargs, pFormat );
-		size_t iRet = VPrintf( pFormat, vargs );
-		va_end( vargs );
-		return( iRet );
-	}
+	/**
+	* @brief Check if EOF is reached.
+	* @return true if EOF is reached, false otherwise.
+	*/
+	bool IsEOF() const;
+	/**
+	* @brief print in file a string with arguments (printf like).
+	* @param pFormat string in "printf like" format.
+	* @param args argument list.
+	* @return total chars of the output.
+	*/
+	size_t VPrintf( LPCTSTR pFormat, va_list args );
+	/**
+	* @brief print in file a string with arguments (printf like).
+	* @param pFormat string in "printf like" format.
+	* @param ... argument list.
+	* @return total chars of the output.
+	*/
+	size_t _cdecl Printf( LPCTSTR pFormat, ... ) __printfargs(2,3);
 };
 
 #endif // _INC_CFILE_H
