40cdc6bc2a80012675b1901277e7a0294c290dad
Azmanomer <omerongul@hotmail.com>
2015-11-19 16:26:27 +0200

    fix for build

--- src/network/send.cpp
+++ src/network/send.cpp
@@ -1243,7 +1243,7 @@ PacketItemContents::PacketItemContents(const CClient* target, const CItem* spell
 	skip(2);
 
 	m_count = 0;
-	for (int i = SPELL_Clumsy; i <= SPELL_BOOK_QTY; i++)
+	for (int i = SPELL_Clumsy; i <= SPELL_MAGERY_QTY; i++)
 	{
 		if (spellbook->IsSpellInBook(static_cast<SPELL_TYPE>(i)) == false)
 			continue;
