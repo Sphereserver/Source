a3a70d7a6f8d8c9b5fe64b197d5f60022345d197
Coruja <coruja@outlook.com>
2016-06-03 12:14:16 -0300

    Fixed compiler broken on Linux

--- src/graysvr/CItemShip.cpp
+++ src/graysvr/CItemShip.cpp
@@ -85,7 +85,7 @@ bool CItemShip::Ship_SetMoveDir(DIR_TYPE dir, BYTE speed, bool bWheelMove)
 	if ( !IsAttr(ATTR_MAGIC) && !Calc_GetRandVal(10) )	// make sound.
 		Sound(Calc_GetRandVal(2) ? 0x12 : 0x13);
 
-	m_itShip.m_fSail = min(speed, 2);		// checking here that packet is legit from client and not modified by 3rd party tools to send speed > 2.
+	m_itShip.m_fSail = minimum(speed, 2);	// checking here that packet is legit from client and not modified by 3rd party tools to send speed > 2.
 	GetTopSector()->SetSectorWakeStatus();	// may get here b4 my client does.
 	CItemMulti *pItemMulti = static_cast<CItemMulti *>(this);
 	pItemMulti->m_SpeedMode = (speed == 1) ? 3 : 4;
