Sphereserver/Source

commit 17496e192535b4cf8cfbc96c659c09fbb9301ccf

Removed redundant functions calls on some skills

src/graysvr/CCharSkill.cpp
@@ -892,6 +892,7 @@ bool CChar::Stats_Regen(INT64 iTimeDiff)
 	}
 	return true;
 }
+
 unsigned short CChar::Stats_GetRegenVal(STAT_TYPE iStat, bool bGetTicks)
 {
 	ADDTOCALLSTACK("CChar::Stats_GetRegenVal");
@@ -1351,10 +1352,11 @@ bool CChar::Skill_MakeItem( ITEMID_TYPE id, CGrayUID uidTarg, SKTRIG_TYPE stage,
 	// SKILL_BLACKSMITHING
 	// SKILL_BOWCRAFT
 	// SKILL_CARPENTRY
+	// SKILL_CARTOGRAPHY
 	// SKILL_COOKING
 	// SKILL_INSCRIPTION
-	// SKILL_TAILORING:
-	// SKILL_TINKERING,
+	// SKILL_TAILORING
+	// SKILL_TINKERING
 	//
 	// Confer the new item.
 	// Test for consumable items.
@@ -2089,22 +2091,6 @@ int CChar::Skill_DetectHidden( SKTRIG_TYPE stage )
 		return -SKTRIG_FAIL;
 
 	return 0;
-}
-
-int CChar::Skill_Cartography( SKTRIG_TYPE stage )
-{
-	ADDTOCALLSTACK("CChar::Skill_Cartography");
-	// SKILL_CARTOGRAPHY
-	// m_atCreate.m_ItemID = map we are making.
-	// m_atCreate.m_Amount = amount of said item.
-
-	if ( stage == SKTRIG_START )
-	{
-		if ( !g_Cfg.IsSkillFlag( Skill_GetActive(), SKF_NOSFX ) )
-			Sound( 0x249 );
-	}
-
-	return( Skill_MakeItem( stage ));
 }
 
 int CChar::Skill_Musicianship( SKTRIG_TYPE stage )
@@ -3172,32 +3158,6 @@ int CChar::Skill_MakeItem( SKTRIG_TYPE stage )
 	return( -SKTRIG_QTY );
 }
 
-int CChar::Skill_Tailoring( SKTRIG_TYPE stage )
-{
-	ADDTOCALLSTACK("CChar::Skill_Tailoring");
-	if ( stage == SKTRIG_SUCCESS )
-	{
-		if ( !g_Cfg.IsSkillFlag( Skill_GetActive(), SKF_NOSFX ) )
-			Sound( SOUND_SNIP );	// snip noise
-	}
-
-	return( Skill_MakeItem( stage ));
-}
-
-int CChar::Skill_Inscription( SKTRIG_TYPE stage )
-{
-	ADDTOCALLSTACK("CChar::Skill_Inscription");
-	if ( stage == SKTRIG_START )
-	{
-		// Can we even attempt to make this scroll ?
-		// m_atCreate.m_ItemID = create this item
-		if ( !g_Cfg.IsSkillFlag( Skill_GetActive(), SKF_NOSFX ) )
-			Sound( 0x249 );
-	}
-
-	return( Skill_MakeItem( stage ));
-}
-
 int CChar::Skill_Blacksmith( SKTRIG_TYPE stage )
 {
 	ADDTOCALLSTACK("CChar::Skill_Blacksmith");
@@ -3651,6 +3611,9 @@ int CChar::Skill_Stage( SKTRIG_TYPE stage )
 			return 0;
 		case SKILL_ALCHEMY:
 		case SKILL_BOWCRAFT:
+		case SKILL_CARTOGRAPHY:
+		case SKILL_INSCRIPTION:
+		case SKILL_TAILORING:
 		case SKILL_TINKERING:
 			return Skill_MakeItem(stage);
 		case SKILL_ANATOMY:
@@ -3669,8 +3632,6 @@ int CChar::Skill_Stage( SKTRIG_TYPE stage )
 			return Skill_Peacemaking(stage);
 		case SKILL_CARPENTRY:
 			return Skill_Carpentry(stage);
-		case SKILL_CARTOGRAPHY:
-			return Skill_Cartography(stage);
 		case SKILL_COOKING:
 			return Skill_Cooking(stage);
 		case SKILL_DETECTINGHIDDEN:
@@ -3688,8 +3649,6 @@ int CChar::Skill_Stage( SKTRIG_TYPE stage )
 			return Skill_Hiding(stage);
 		case SKILL_PROVOCATION:
 			return Skill_Provocation(stage);
-		case SKILL_INSCRIPTION:
-			return Skill_Inscription(stage);
 		case SKILL_LOCKPICKING:
 			return Skill_Lockpicking(stage);
 		case SKILL_MAGERY:
@@ -3717,8 +3676,6 @@ int CChar::Skill_Stage( SKTRIG_TYPE stage )
 			return Skill_SpiritSpeak(stage);
 		case SKILL_STEALING:
 			return Skill_Stealing(stage);
-		case SKILL_TAILORING:
-			return Skill_Tailoring(stage);
 		case SKILL_TAMING:
 			return Skill_Taming(stage);
 		case SKILL_TRACKING:
@@ -3819,19 +3776,6 @@ void CChar::Skill_Fail( bool fCancel )
 	Skill_Cleanup();
 }
 
-TRIGRET_TYPE CChar::Skill_OnTrigger( SKILL_TYPE skill, SKTRIG_TYPE stage ) 
-{
-	CScriptTriggerArgs pArgs;
-	return Skill_OnTrigger(skill, stage, &pArgs);
-}
-
-TRIGRET_TYPE CChar::Skill_OnCharTrigger( SKILL_TYPE skill, CTRIG_TYPE stage ) 
-{
-	CScriptTriggerArgs pArgs;
-	return Skill_OnCharTrigger(skill, stage, &pArgs);
-}
-
-
 TRIGRET_TYPE CChar::Skill_OnTrigger( SKILL_TYPE skill, SKTRIG_TYPE  stage, CScriptTriggerArgs *pArgs )
 {
 	ADDTOCALLSTACK("CChar::Skill_OnTrigger");
@@ -3874,7 +3818,6 @@ TRIGRET_TYPE CChar::Skill_OnCharTrigger( SKILL_TYPE skill, CTRIG_TYPE ctrig, CSc
 
 	return OnTrigger(ctrig, this, pArgs);
 }
-
 
 int CChar::Skill_Done()
 {

src/graysvr/CObjBase.h
@@ -3702,11 +3702,8 @@ public:
 	ANIM_TYPE Skill_GetAnim( SKILL_TYPE skill);
 	SOUND_TYPE Skill_GetSound( SKILL_TYPE skill);
 	int Skill_Stage( SKTRIG_TYPE stage );
-	TRIGRET_TYPE	Skill_OnTrigger( SKILL_TYPE skill, SKTRIG_TYPE  stage);
-	TRIGRET_TYPE	Skill_OnTrigger( SKILL_TYPE skill, SKTRIG_TYPE  stage, CScriptTriggerArgs * pArgs); //pArgs.m_iN1 will be rewritten with skill
-
-	TRIGRET_TYPE	Skill_OnCharTrigger( SKILL_TYPE skill, CTRIG_TYPE ctrig);
-	TRIGRET_TYPE	Skill_OnCharTrigger( SKILL_TYPE skill, CTRIG_TYPE ctrig, CScriptTriggerArgs * pArgs); //pArgs.m_iN1 will be rewritten with skill
+	TRIGRET_TYPE Skill_OnTrigger( SKILL_TYPE skill, SKTRIG_TYPE  stage, CScriptTriggerArgs *pArgs = NULL);		//pArgs.m_iN1 will be rewritten with skill
+	TRIGRET_TYPE Skill_OnCharTrigger( SKILL_TYPE skill, CTRIG_TYPE ctrig, CScriptTriggerArgs *pArgs = NULL);	//pArgs.m_iN1 will be rewritten with skill
 
 	bool Skill_Mining_Smelt( CItem * pItemOre, CItem * pItemTarg );
 	bool Skill_Tracking( CGrayUID uidTarg, DIR_TYPE & dirPrv, int iDistMax = SHRT_MAX );
@@ -3733,7 +3730,6 @@ private:
 
 	int	Skill_Scripted( SKTRIG_TYPE stage );
 
-	int Skill_Inscription( SKTRIG_TYPE stage );
 	int Skill_MakeItem( SKTRIG_TYPE stage );
 	int Skill_Information( SKTRIG_TYPE stage );
 	int Skill_Hiding( SKTRIG_TYPE stage );
@@ -3744,7 +3740,6 @@ private:
 	int Skill_Lumberjack( SKTRIG_TYPE stage );
 	int Skill_Taming( SKTRIG_TYPE stage );
 	int Skill_Fishing( SKTRIG_TYPE stage );
-	int Skill_Cartography(SKTRIG_TYPE stage);
 	int Skill_DetectHidden(SKTRIG_TYPE stage);
 	int Skill_Herding(SKTRIG_TYPE stage);
 	int Skill_Blacksmith(SKTRIG_TYPE stage);
@@ -3763,7 +3758,6 @@ private:
 	int Skill_Tracking( SKTRIG_TYPE stage );
 	int Skill_Fighting( SKTRIG_TYPE stage );
 	int Skill_Musicianship( SKTRIG_TYPE stage );
-	int Skill_Tailoring( SKTRIG_TYPE stage );
 
 	int Skill_Act_Napping(SKTRIG_TYPE stage);
 	int Skill_Act_Throwing(SKTRIG_TYPE stage);

commit d321dc50b19387798bf7984548932e786864e52f

Fixed console exception caused by commit https://github.com/Sphereserver/Source/commit/17496e192535b4cf8cfbc96c659c09fbb9301ccf

src/graysvr/CCharSkill.cpp
@@ -3783,9 +3783,12 @@ TRIGRET_TYPE CChar::Skill_OnTrigger( SKILL_TYPE skill, SKTRIG_TYPE  stage, CScri
 	if ( !(stage == SKTRIG_SELECT || stage == SKTRIG_GAIN || stage == SKTRIG_USEQUICK || stage == SKTRIG_WAIT || stage == SKTRIG_TARGETCANCEL) )
 		m_Act_SkillCurrent = skill;
 
-	pArgs->m_iN1 = skill;
-	if ( g_Cfg.IsSkillFlag(skill, SKF_MAGIC) )
-		pArgs->m_VarsLocal.SetNum("spell", m_atMagery.m_Spell, true);
+	if ( pArgs )
+	{
+		pArgs->m_iN1 = skill;
+		if ( g_Cfg.IsSkillFlag(skill, SKF_MAGIC) )
+			pArgs->m_VarsLocal.SetNum("spell", m_atMagery.m_Spell, true);
+	}
 
 	TRIGRET_TYPE iRet = TRIGRET_RET_DEFAULT;
 
@@ -3810,9 +3813,12 @@ TRIGRET_TYPE CChar::Skill_OnCharTrigger( SKILL_TYPE skill, CTRIG_TYPE ctrig, CSc
 	if ( !(ctrig == CTRIG_SkillSelect || ctrig == CTRIG_SkillGain || ctrig == CTRIG_SkillUseQuick || ctrig == CTRIG_SkillWait || ctrig == CTRIG_SkillTargetCancel) )
 		m_Act_SkillCurrent = skill;
 
-	pArgs->m_iN1 = skill;
-	if ( g_Cfg.IsSkillFlag(skill, SKF_MAGIC) )
-		pArgs->m_VarsLocal.SetNum("spell", m_atMagery.m_Spell, true);
+	if ( pArgs )
+	{
+		pArgs->m_iN1 = skill;
+		if ( g_Cfg.IsSkillFlag(skill, SKF_MAGIC) )
+			pArgs->m_VarsLocal.SetNum("spell", m_atMagery.m_Spell, true);
+	}
 
 	return OnTrigger(ctrig, this, pArgs);
 }