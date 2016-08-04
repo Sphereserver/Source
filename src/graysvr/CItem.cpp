Sphereserver/Source

commit 6495d71b46ba6e3caf5446d70fac5535ae16a23e

Fixed non-pointer variables being changed to NULL Although some compilers accept this, some others will return error/warning when the code set a NULL value on something that must be numeric (eg: int something = NULL) Sphere was doing this on GetDefNum() and GetKeyNum(), I just changed these functions to return 0 instead NULL

src/graysvr/CItem.cpp
@@ -266,7 +266,7 @@ CItem * CItem::GenerateScript( CChar * pSrc)
 				int iBlood = 0;
 				if ( pItemDef )
 				{
-					iBlood = static_cast<int>(pItemDef->m_TagDefs.GetKeyNum("MAXBLOOD", true));
+					iBlood = static_cast<int>(pItemDef->m_TagDefs.GetKeyNum("MAXBLOOD"));
 				}
 				if ( !iBlood )
 					iBlood = 5;
@@ -1173,7 +1173,7 @@ SOUND_TYPE CItem::GetDropSound(const CObjBase *pObjOn) const
 {
 	ADDTOCALLSTACK("CItem::GetDropSound");
 	// Try get drop sound from item
-	SOUND_TYPE iSnd = static_cast<SOUND_TYPE>(GetDefNum("DROPSOUND", true, true));
+	SOUND_TYPE iSnd = static_cast<SOUND_TYPE>(GetDefNum("DROPSOUND", true));
 	if ( iSnd )
 		return iSnd;
 
@@ -3576,7 +3576,7 @@ bool CItem::Use_Portculis()
 {
 	ADDTOCALLSTACK("CItem::Use_Portculis");
 	// Set to the new z location.
-	if ( ! IsTopLevel())
+	if ( !IsTopLevel() )
 		return false;
 
 	CPointMap pt = GetTopPoint();
@@ -3585,28 +3585,16 @@ bool CItem::Use_Portculis()
 	else
 		pt.m_z = static_cast<signed char>(m_itPortculis.m_z1);
 
-	if ( pt.m_z == GetTopZ())
+	if ( pt.m_z == GetTopZ() )
 		return false;
 
-	MoveToUpdate( pt );
+	SOUND_TYPE iSnd = static_cast<SOUND_TYPE>(GetDefNum("PORTCULISSOUND"));
+	if ( !iSnd )
+		iSnd = 0x21D;
 
-	SOUND_TYPE iSnd = 0x21d;
-	if (GetDefNum("PORTCULISSOUND"))
-		iSnd = static_cast<SOUND_TYPE>(GetDefNum("PORTCULISSOUND"));
-	/*CVarDefCont * pTagStorage = NULL; 
-	pTagStorage = GetKey("OVERRIDE.PORTCULISSOUND", true);
-	if ( pTagStorage )
-	{
-		if ( pTagStorage->GetValNum() )
-			iSnd = static_cast<SOUND_TYPE>(pTagStorage->GetValNum());
-		else
-			iSnd = 0x21d;
-	} else 
-		iSnd = 0x21d;*/
-
-	Sound( iSnd );
-
-	return( true );
+	MoveToUpdate(pt);
+	Sound(iSnd);
+	return true;
 }
 
 SOUND_TYPE CItem::Use_Music( bool fWell ) const
@@ -3626,58 +3614,70 @@ bool CItem::Use_DoorNew( bool bJustOpen )
 {
 	ADDTOCALLSTACK("CItem::Use_DoorNew");
 
-	if (! IsTopLevel())
-		return( false );
+	if ( !IsTopLevel() )
+		return false;
 
 	bool bClosing = IsAttr(ATTR_OPENED);
 	if ( bJustOpen && bClosing )
-		return( true );	// links just open
+		return true;	// links just open
 
-	CItemBase * pItemDef = Item_GetDef();
+	CItemBase *pItemDef = Item_GetDef();
+	ITEMID_TYPE idSwitch = static_cast<ITEMID_TYPE>(GetDefNum("DOOROPENID"));
+	if ( !idSwitch )
+	{
+		idSwitch = pItemDef->m_ttDoor.m_idSwitch;
+		if ( !idSwitch )
+			return Use_Door(bJustOpen);
+	}
 
-	//default or override ID
-	ITEMID_TYPE idSwitch = GetDefNum("DOOROPENID") ? static_cast<ITEMID_TYPE>(GetDefNum("DOOROPENID", true)) : pItemDef->m_ttDoor.m_idSwitch;
-	if (!idSwitch)
-		return Use_Door(bJustOpen);
-
-	//default or override locations
-	short sDifX = m_itNormal.m_morep.m_x ? m_itNormal.m_morep.m_x : static_cast<short>(pItemDef->m_ttDoor.m_iXChange);
-	short sDifY = m_itNormal.m_morep.m_y ? m_itNormal.m_morep.m_y : static_cast<short>(pItemDef->m_ttDoor.m_iYChange);
-
-	
-	//default sounds
-	SOUND_TYPE iCloseSnd = pItemDef->m_ttDoor.m_iSoundClose ? pItemDef->m_ttDoor.m_iSoundClose : 0x00f1;
-	SOUND_TYPE iOpenSnd = pItemDef->m_ttDoor.m_iSoundOpen ? pItemDef->m_ttDoor.m_iSoundOpen : 0x00ea;
-
-	//override sounds
-	if (GetDefNum("DOORCLOSESOUND"))
-		iCloseSnd = static_cast<SOUND_TYPE>(GetDefNum("DOORCLOSESOUND"));
-	if (GetDefNum("DOOROPENSOUND"))
-		iOpenSnd = static_cast<SOUND_TYPE>(GetDefNum("DOOROPENSOUND"));
+	short sDifX = m_itNormal.m_morep.m_x;
+	if ( !sDifX )
+		sDifX = static_cast<short>(pItemDef->m_ttDoor.m_iXChange);
+	short sDifY = m_itNormal.m_morep.m_y;
+	if ( !sDifY )
+		sDifY = static_cast<short>(pItemDef->m_ttDoor.m_iYChange);
 
 	CPointMap pt = GetTopPoint();
-	if (bClosing)
+	SOUND_TYPE iSnd;
+	if ( bClosing )
 	{
+		ClrAttr(ATTR_OPENED);
+		SetTimeout(-1);
 		pt.m_x -= sDifX;
 		pt.m_y -= sDifY;
+
+		iSnd = static_cast<SOUND_TYPE>(GetDefNum("DOORCLOSESOUND"));
+		if ( !iSnd )
+		{
+			iSnd = pItemDef->m_ttDoor.m_iSoundClose;
+			if ( !iSnd )
+				iSnd = 0xF1;
+		}
 	}
 	else
 	{
+		SetAttr(ATTR_OPENED);
+		SetTimeout(20 * TICK_PER_SEC);
 		pt.m_x += sDifX;
 		pt.m_y += sDifY;
+
+		iSnd = static_cast<SOUND_TYPE>(GetDefNum("DOOROPENSOUND"));
+		if ( !iSnd )
+		{
+			iSnd = pItemDef->m_ttDoor.m_iSoundOpen;
+			if ( !iSnd )
+				iSnd = 0xEA;
+		}
 	}
 
 	SetDefNum("DOOROPENID", GetDispID());
 	SetDispID(idSwitch);
-
 	MoveToUpdate(pt);
-	Sound( bClosing ? iCloseSnd : iOpenSnd );
-	SetTimeout( bClosing ? -1 : 20*TICK_PER_SEC );
-	bClosing ? ClrAttr(ATTR_OPENED) : SetAttr(ATTR_OPENED);
-	return( ! bClosing );
+	Sound(iSnd);
+	return !bClosing;
 }
 
-bool CItem::Use_Door( bool fJustOpen )
+bool CItem::Use_Door( bool bJustOpen )
 {
 	ADDTOCALLSTACK("CItem::Use_Door");
 	// don't call this directly but call CChar::Use_Item() instead.
@@ -3686,16 +3686,15 @@ bool CItem::Use_Door( bool fJustOpen )
 	//  true = open
 
 	ITEMID_TYPE id = GetDispID();
-	int doordir = CItemBase::IsID_Door( id )-1;
-	if ( doordir < 0 || ! IsTopLevel())
-		return( false );
+	int doordir = CItemBase::IsID_Door(id) - 1;
+	if ( !IsTopLevel() || (doordir < 0) )
+		return false;
 
 	id = static_cast<ITEMID_TYPE>(id - doordir);
-	// IT_TYPE typelock = m_type;
 
-	bool fClosing = ( doordir & DOOR_OPENED );	// currently open
-	if ( fJustOpen && fClosing )
-		return( true );	// links just open
+	bool bClosing = (doordir & DOOR_OPENED);	// currently open
+	if ( bJustOpen && bClosing )
+		return true;	// links just open
 
 	CPointMap pt = GetTopPoint();
 	switch ( doordir )
@@ -3774,56 +3773,72 @@ bool CItem::Use_Door( bool fJustOpen )
 			break;
 	}
 
-	SetDispID(static_cast<ITEMID_TYPE>(id + doordir));
-	// SetType( typelock );	// preserve the fact that it was locked.
-	MoveToUpdate(pt);
-
-	//CVarDefCont * pTagStorage = NULL; 
-	SOUND_TYPE iCloseSnd = 0x00f1;
-	SOUND_TYPE iOpenSnd = 0x00ea;
-
-	switch ( id )
+	SOUND_TYPE iSnd;
+	if ( bClosing )
 	{
-		case ITEMID_DOOR_SECRET_1:
-		case ITEMID_DOOR_SECRET_2:
-		case ITEMID_DOOR_SECRET_3:
-		case ITEMID_DOOR_SECRET_4:
-		case ITEMID_DOOR_SECRET_5:
-		case ITEMID_DOOR_SECRET_6:
-			iCloseSnd = 0x002e;
-			iOpenSnd = 0x002f;
-			break;
-		case ITEMID_DOOR_METAL_S:
-		case ITEMID_DOOR_BARRED:
-		case ITEMID_DOOR_METAL_L:
-		case ITEMID_DOOR_IRONGATE_1:
-		case ITEMID_DOOR_IRONGATE_2:
-			iCloseSnd = 0x00f3;
-			iOpenSnd = 0x00eb;
-			break;
-		default:
-			break;
+		SetTimeout(-1);
+
+		iSnd = static_cast<SOUND_TYPE>(GetDefNum("DOORCLOSESOUND"));
+		if ( !iSnd )
+		{
+			switch ( id )
+			{
+				case ITEMID_DOOR_SECRET_1:
+				case ITEMID_DOOR_SECRET_2:
+				case ITEMID_DOOR_SECRET_3:
+				case ITEMID_DOOR_SECRET_4:
+				case ITEMID_DOOR_SECRET_5:
+				case ITEMID_DOOR_SECRET_6:
+					iSnd = 0x2E;
+					break;
+				case ITEMID_DOOR_METAL_S:
+				case ITEMID_DOOR_BARRED:
+				case ITEMID_DOOR_METAL_L:
+				case ITEMID_DOOR_IRONGATE_1:
+				case ITEMID_DOOR_IRONGATE_2:
+					iSnd = 0xF3;
+					break;
+				default:
+					iSnd = 0xF1;
+					break;
+			}
+		}
+	}
+	else
+	{
+		SetTimeout(60 * TICK_PER_SEC);
+
+		iSnd = static_cast<SOUND_TYPE>(GetDefNum("DOOROPENSOUND"));
+		if ( !iSnd )
+		{
+			switch ( id )
+			{
+				case ITEMID_DOOR_SECRET_1:
+				case ITEMID_DOOR_SECRET_2:
+				case ITEMID_DOOR_SECRET_3:
+				case ITEMID_DOOR_SECRET_4:
+				case ITEMID_DOOR_SECRET_5:
+				case ITEMID_DOOR_SECRET_6:
+					iSnd = 0x2F;
+					break;
+				case ITEMID_DOOR_METAL_S:
+				case ITEMID_DOOR_BARRED:
+				case ITEMID_DOOR_METAL_L:
+				case ITEMID_DOOR_IRONGATE_1:
+				case ITEMID_DOOR_IRONGATE_2:
+					iSnd = 0xEB;
+					break;
+				default:
+					iSnd = 0xEA;
+					break;
+			}
+		}
 	}
 
-	//override sounds
-	if (GetDefNum("DOORCLOSESOUND"))
-		iCloseSnd = static_cast<SOUND_TYPE>(GetDefNum("DOORCLOSESOUND"));
-	if (GetDefNum("DOOROPENSOUND"))
-		iOpenSnd = static_cast<SOUND_TYPE>(GetDefNum("DOOROPENSOUND"));
-
-	/*pTagStorage = GetKey("OVERRIDE.DOORSOUND_CLOSE", true);
-	if ( pTagStorage )
-		iCloseSnd = static_cast<SOUND_TYPE>(pTagStorage->GetValNum());
-	pTagStorage = NULL;
-	pTagStorage = GetKey("OVERRIDE.DOORSOUND_OPEN", true);
-	if ( pTagStorage )
-		iOpenSnd = static_cast<SOUND_TYPE>(pTagStorage->GetValNum());*/
-
-	Sound( fClosing ? iCloseSnd : iOpenSnd );
-
-	// Auto close the door in n seconds.
-	SetTimeout( fClosing ? -1 : 60*TICK_PER_SEC );
-	return( ! fClosing );
+	SetDispID(static_cast<ITEMID_TYPE>(id + doordir));
+	MoveToUpdate(pt);
+	Sound(iSnd);
+	return !bClosing;
 }
 
 bool CItem::Armor_IsRepairable() const
@@ -3923,13 +3938,11 @@ SKILL_TYPE CItem::Weapon_GetSkill() const
 	CItemBase * pItemDef = Item_GetDef();
 	ASSERT(pItemDef);
 
-	int iSkillOverride = static_cast<int>(m_TagDefs.GetKeyNum("OVERRIDE_SKILL", true) - 1);
-	if ( iSkillOverride == -1)
-		iSkillOverride = static_cast<int>(m_TagDefs.GetKeyNum("OVERRIDE.SKILL", true) - 1);
-	if ( iSkillOverride > SKILL_NONE && iSkillOverride < static_cast<int>(g_Cfg.m_iMaxSkill) )
+	unsigned int iSkillOverride = static_cast<unsigned int>(m_TagDefs.GetKeyNum("OVERRIDE.SKILL"));
+	if ( (iSkillOverride > SKILL_NONE) && (iSkillOverride < g_Cfg.m_iMaxSkill) )
 		return static_cast<SKILL_TYPE>(iSkillOverride);
 
-	if ( pItemDef->m_iSkill > SKILL_NONE && pItemDef->m_iSkill < static_cast<SKILL_TYPE>(g_Cfg.m_iMaxSkill) )
+	if ( (pItemDef->m_iSkill > SKILL_NONE) && (pItemDef->m_iSkill < static_cast<SKILL_TYPE>(g_Cfg.m_iMaxSkill)) )
 		return pItemDef->m_iSkill;
 
 	switch ( pItemDef->GetType() )
@@ -4163,7 +4176,7 @@ bool CItem::Use_Light()
 	if ( IsType(IT_LIGHT_OUT) && (m_itLight.m_burned || IsItemInContainer()) )
 		return false;
 
-	ITEMID_TYPE id = static_cast<ITEMID_TYPE>(m_TagDefs.GetKeyNum("OVERRIDE_LIGHTID", true));
+	ITEMID_TYPE id = static_cast<ITEMID_TYPE>(m_TagDefs.GetKeyNum("OVERRIDE_LIGHTID"));
 	if ( !id )
 	{
 		id = static_cast<ITEMID_TYPE>(Item_GetDef()->m_ttEquippable.m_Light_ID.GetResIndex());
@@ -4609,7 +4622,7 @@ int CItem::OnTakeDamage( int iDmg, CChar * pSrc, DAMAGE_TYPE uType )
 	if ( iDmg <= 0 )
 		return( 0 );
 
-	INT64 iSelfRepair = GetDefNum("SELFREPAIR", true, true);
+	INT64 iSelfRepair = GetDefNum("SELFREPAIR", true);
 	if ( iSelfRepair > Calc_GetRandVal(10) )
 	{
 		m_itArmor.m_Hits_Cur += 2;

src/common/CRegion.h
@@ -547,14 +547,14 @@ public:
 	TRIGRET_TYPE OnRegionTrigger( CTextConsole * pChar, RTRIG_TYPE trig );
 
 public:
-	LPCTSTR GetDefStr( LPCTSTR pszKey, bool fZero = false ) const
+	LPCTSTR GetDefStr(LPCTSTR pszKey, bool fZero = false) const
 	{
-		return m_BaseDefs.GetKeyStr( pszKey, fZero );
+		return m_BaseDefs.GetKeyStr(pszKey, fZero);
 	}
 
-	INT64 GetDefNum( LPCTSTR pszKey, bool fZero = false ) const
+	INT64 GetDefNum(LPCTSTR pszKey) const
 	{
-		return m_BaseDefs.GetKeyNum( pszKey, fZero );
+		return m_BaseDefs.GetKeyNum(pszKey);
 	}
 
 	void SetDefNum(LPCTSTR pszKey, INT64 iVal, bool fZero = true)

src/common/CVarDefMap.cpp
@@ -612,16 +612,14 @@ CVarDefCont * CVarDefMap::GetKey( LPCTSTR pszKey ) const
 	return( pReturn );
 }
 
-INT64 CVarDefMap::GetKeyNum( LPCTSTR pszKey, bool fZero  ) const
+INT64 CVarDefMap::GetKeyNum( LPCTSTR pszKey ) const
 {
 	ADDTOCALLSTACK("CVarDefMap::GetKeyNum");
-	CVarDefCont * pVar = GetKey(pszKey);
-	if ( pVar == NULL )
-		return (fZero ? 0 : NULL);
-	return pVar->GetValNum();
+	CVarDefCont *pVar = GetKey(pszKey);
+	return pVar ? pVar->GetValNum() : 0;
 }
 
-LPCTSTR CVarDefMap::GetKeyStr( LPCTSTR pszKey, bool fZero  ) const
+LPCTSTR CVarDefMap::GetKeyStr( LPCTSTR pszKey, bool fZero ) const
 {
 	ADDTOCALLSTACK("CVarDefMap::GetKeyStr");
 	CVarDefCont * pVar = GetKey(pszKey);

src/common/CVarDefMap.h
@@ -153,7 +153,7 @@ public:
 
 	CVarDefCont * GetAt( size_t at ) const;
 	CVarDefCont * GetKey( LPCTSTR pszKey ) const;
-	INT64 GetKeyNum( LPCTSTR pszKey, bool fZero = false ) const;
+	INT64 GetKeyNum( LPCTSTR pszKey ) const;
 	LPCTSTR GetKeyStr( LPCTSTR pszKey, bool fZero = false ) const;
 	CVarDefCont * GetParseKey( LPCTSTR & pArgs ) const;
 	CVarDefCont * CheckParseKey( LPCTSTR & pszArgs ) const;

src/graysvr/CAccount.h
@@ -87,8 +87,14 @@ private:
 	CAccount& operator=(const CAccount& other);
 public:
 
-	LPCTSTR GetDefStr( LPCTSTR pszKey, bool fZero = false ) const { return m_BaseDefs.GetKeyStr( pszKey, fZero ); }
-	INT64 GetDefNum( LPCTSTR pszKey, bool fZero = false ) const	{ return m_BaseDefs.GetKeyNum( pszKey, fZero );	}
+	LPCTSTR GetDefStr(LPCTSTR pszKey, bool fZero = false) const
+	{
+		return m_BaseDefs.GetKeyStr(pszKey, fZero);
+	}
+	INT64 GetDefNum(LPCTSTR pszKey) const
+	{
+		return m_BaseDefs.GetKeyNum(pszKey);
+	}
 	void SetDefNum(LPCTSTR pszKey, INT64 iVal, bool fZero = true) 
 	{ 
 		m_BaseDefs.SetNum(pszKey, iVal, fZero);
@@ -97,7 +103,10 @@ public:
 	{
 		m_BaseDefs.SetStr(pszKey, fQuoted, pszVal, fZero);
 	}
-	void DeleteDef(LPCTSTR pszKey) { m_BaseDefs.DeleteKey(pszKey); }
+	void DeleteDef(LPCTSTR pszKey)
+	{
+		m_BaseDefs.DeleteKey(pszKey);
+	}
 
 	
 	/**

src/graysvr/CBase.cpp
@@ -145,7 +145,7 @@ bool CBaseBaseDef::r_WriteVal( LPCTSTR pszKey, CGString & sVal, CTextConsole * p
 		case OBC_WEIGHTREDUCTION:
 		case OBC_COMBATBONUSSTAT:
 		case OBC_COMBATBONUSPERCENT:
-			sVal.FormatLLVal(GetDefNum(pszKey, true));
+			sVal.FormatLLVal(GetDefNum(pszKey));
 			break;
 
 		case OBC_DEFNAME:

src/graysvr/CBase.h
@@ -44,14 +44,14 @@ public:
 	DWORD	m_Can;			// Base attribute flags. CAN_C_GHOST
 
 public:
-	LPCTSTR GetDefStr( LPCTSTR pszKey, bool fZero = false ) const
+	LPCTSTR GetDefStr(LPCTSTR pszKey, bool fZero = false) const
 	{
-		return m_BaseDefs.GetKeyStr( pszKey, fZero );
+		return m_BaseDefs.GetKeyStr(pszKey, fZero);
 	}
 
-	INT64 GetDefNum( LPCTSTR pszKey, bool fZero = false ) const
+	INT64 GetDefNum(LPCTSTR pszKey) const
 	{
-		return m_BaseDefs.GetKeyNum( pszKey, fZero );
+		return m_BaseDefs.GetKeyNum(pszKey);
 	}
 
 	void SetDefNum(LPCTSTR pszKey, INT64 iVal, bool fZero = true)
@@ -192,16 +192,14 @@ public:
 		return( m_sName.IsValid());
 	}
 
-	BYTE	RangeL() const
+	BYTE RangeL() const
 	{
-		return static_cast<BYTE>(GetDefNum("RANGE",true) & 0xff);
-		//return (m_range & 0xff);
+		return static_cast<BYTE>(GetDefNum("RANGE") & 0xff);
 	}
 
-	BYTE	RangeH() const
+	BYTE RangeH() const
 	{
-		return static_cast<BYTE>((GetDefNum("RANGE",true)>>8) & 0xff);
-		//return ((m_range>>8) & 0xff);
+		return static_cast<BYTE>((GetDefNum("RANGE") >> 8) & 0xff);
 	}
 
 	height_t GetHeight() const

src/graysvr/CChar.cpp
@@ -1774,10 +1774,6 @@ do_default:
 
 	switch ( iKeyNum )
 	{
-		//return as decimal number or 0 if not set
-		case CHC_CURFOLLOWER:
-			sVal.FormatLLVal(GetDefNum(pszKey,true));
-			break;
 		//On these ones, check BaseDef if not found on dynamic
 		case CHC_FASTERCASTRECOVERY:
 		case CHC_FASTERCASTING:
@@ -1790,6 +1786,7 @@ do_default:
 		case CHC_LOWERMANACOST:
 		case CHC_LOWERREAGENTCOST:
 		case CHC_LUCK:
+		case CHC_CURFOLLOWER:
 		case CHC_MAXFOLLOWER:
 		case CHC_RESPHYSICAL:
 		case CHC_RESPHYSICALMAX:
@@ -1811,7 +1808,7 @@ do_default:
 		case CHC_REGENVALMANA:
 		case CHC_SPELLTIMEOUT:
 		case CHC_TITHING:
-			sVal.FormatLLVal(GetDefNum(pszKey, true));
+			sVal.FormatLLVal(GetDefNum(pszKey));
 			break;
 		case CHC_ATTACKER:
 			{

commit f6538af2ddc8819abae97dd67b1d58c09fdc6da8

Fixed weapons not using correct skills This was caused by commit https://github.com/Sphereserver/Source/commit/6495d71b46ba6e3caf5446d70fac5535ae16a23e

src/graysvr/CItem.cpp
@@ -3935,36 +3935,39 @@ SKILL_TYPE CItem::Weapon_GetSkill() const
 	ADDTOCALLSTACK("CItem::Weapon_GetSkill");
 	// assuming this is a weapon. What skill does it apply to.
 
-	CItemBase * pItemDef = Item_GetDef();
+	CVarDefCont *pVar = m_TagDefs.GetKey("OVERRIDE.SKILL");
+	if ( pVar )
+	{
+		SKILL_TYPE iSkillOverride = static_cast<SKILL_TYPE>(pVar->GetValNum());
+		if ( (iSkillOverride > SKILL_NONE) && (iSkillOverride < static_cast<SKILL_TYPE>(g_Cfg.m_iMaxSkill)) )
+			return iSkillOverride;
+	}
+
+	CItemBase *pItemDef = Item_GetDef();
 	ASSERT(pItemDef);
-
-	int iSkillOverride = static_cast<int>(m_TagDefs.GetKeyNum("OVERRIDE.SKILL"));
-	if ( (iSkillOverride > SKILL_NONE) && (iSkillOverride < static_cast<int>(g_Cfg.m_iMaxSkill)) )
-		return static_cast<SKILL_TYPE>(iSkillOverride);
-
 	if ( (pItemDef->m_iSkill > SKILL_NONE) && (pItemDef->m_iSkill < static_cast<SKILL_TYPE>(g_Cfg.m_iMaxSkill)) )
 		return pItemDef->m_iSkill;
 
 	switch ( pItemDef->GetType() )
 	{
+		case IT_WEAPON_MACE_SMITH:
+		case IT_WEAPON_MACE_SHARP:
+		case IT_WEAPON_MACE_STAFF:
 		case IT_WEAPON_MACE_CROOK:
 		case IT_WEAPON_MACE_PICK:
-		case IT_WEAPON_MACE_SMITH:	// Can be used for smithing ?
-		case IT_WEAPON_MACE_STAFF:
-		case IT_WEAPON_MACE_SHARP:	// war axe can be used to cut/chop trees.
-			return( SKILL_MACEFIGHTING );
+			return SKILL_MACEFIGHTING;
 		case IT_WEAPON_SWORD:
 		case IT_WEAPON_AXE:
-			return( SKILL_SWORDSMANSHIP );
+			return SKILL_SWORDSMANSHIP;
 		case IT_WEAPON_FENCE:
-			return( SKILL_FENCING );
+			return SKILL_FENCING;
 		case IT_WEAPON_BOW:
 		case IT_WEAPON_XBOW:
-			return( SKILL_ARCHERY );
+			return SKILL_ARCHERY;
 		case IT_WEAPON_THROWING:
-			return ( SKILL_THROWING );
+			return SKILL_THROWING;
 		default:
-			return( SKILL_WRESTLING );
+			return SKILL_WRESTLING;
 	}
 }
 