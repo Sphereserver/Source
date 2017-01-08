5cb0bf380a63338ad3ddd190fb2c7da008fe2dfa
Coruja <coruja@outlook.com>
2017-01-08 16:09:45 -0200

    Trying again

--- src/common/libev/ev.c
+++ src/common/libev/ev.c
@@ -1828,11 +1828,11 @@ typedef struct
   #include "ev_wrap.h"
 
   static struct ev_loop default_loop_struct;
-  EV_API_DECL struct ev_loop *ev_default_loop_ptr; /* needs to be initialised to make it a definition despite extern */
+  struct ev_loop *ev_default_loop_ptr = 0; /* needs to be initialised to make it a definition despite extern */
 
 #else
 
-  EV_API_DECL ev_tstamp ev_rt_now; /* needs to be initialised to make it a definition despite extern */
+  ev_tstamp ev_rt_now = 0; /* needs to be initialised to make it a definition despite extern */
   #define VAR(name,decl) static decl;
     #include "ev_vars.h"
   #undef VAR
