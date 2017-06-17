Sphereserver/Source

commit 7a18fdc4cc06147d3cef8d78dd6f77bf22693d5e

[sphere.ini]: Removed unused 'AutoPrivFlags' setting and updated description of NPCAI flags

src/graysvr/CAccount.cpp
@@ -551,9 +551,7 @@ CAccount::CAccount( LPCTSTR pszName, bool fGuest )
 	else
 		SetPrivLevel(PLEVEL_Player);
 
-	m_PrivFlags = static_cast<WORD>(g_Cfg.m_iAutoPrivFlags);
 	m_MaxChars = 0;
-
 	m_Total_Connect_Time = 0;
 	m_Last_Connect_Time = 0;
 	// Add myself to the list.

src/graysvr/CResource.cpp
@@ -171,7 +171,6 @@ CResource::CResource()
 	m_iMaxLoopTimes = 100000;
 
 	m_bAutoResDisp = true;
-	m_iAutoPrivFlags = PRIV_DETAIL;
 
 	// Third Party Tools
 	m_fCUOStatus = true;
@@ -372,7 +371,6 @@ enum RC_TYPE
 	RC_ATTACKERTIMEOUT,			// m_iAttackerTimeout
 	RC_ATTACKINGISACRIME,		// m_fAttackingIsACrime
 	RC_AUTONEWBIEKEYS,			// m_fAutoNewbieKeys
-	RC_AUTOPRIVFLAGS,			// m_iAutoPrivFlags
 	RC_AUTORESDISP,				// m_bAutoResDisp
 	RC_AUTOTOOLTIPRESEND,		// m_iAutoTooltipResend
 	RC_BACKUPLEVELS,			// m_iSaveBackupLevels
@@ -597,7 +595,6 @@ const CAssocReg CResource::sm_szLoadKeys[RC_QTY+1] =
 	{ "ATTACKERTIMEOUT",		{ ELEM_INT,		OFFSETOF(CResource,m_iAttackerTimeout),		0 }},
 	{ "ATTACKINGISACRIME",		{ ELEM_BOOL,	OFFSETOF(CResource,m_fAttackingIsACrime),	0 }},
 	{ "AUTONEWBIEKEYS",			{ ELEM_BOOL,	OFFSETOF(CResource,m_fAutoNewbieKeys),		0 }},
-	{ "AUTOPRIVFLAGS",			{ ELEM_INT,		OFFSETOF(CResource,m_iAutoPrivFlags),		0 }},
 	{ "AUTORESDISP",			{ ELEM_BOOL,	OFFSETOF(CResource,m_bAutoResDisp),			0 }},
 	{ "AUTOTOOLTIPRESEND",		{ ELEM_INT,		OFFSETOF(CResource,m_iAutoTooltipResend),	0 }},
 	{ "BACKUPLEVELS",			{ ELEM_INT,		OFFSETOF(CResource,m_iSaveBackupLevels),	0 }},

src/graysvr/CResource.h
@@ -917,18 +917,16 @@ public:
 #define STAT_FLAG_DENYMAXN	0x04	//		.. for npcs
 	int		m_iStatFlag;
 
-#define NPC_AI_PATH				0x00001		//	NPC pathfinding
-#define	NPC_AI_FOOD				0x00002		//	NPC food search (objects + grass)
-#define	NPC_AI_EXTRA			0x00004		//	NPC magics, combat, etc
-#define NPC_AI_ALWAYSINT		0x00008		//	NPC pathfinding does not check int, always smart
-#define NPC_AI_INTFOOD			0x00010		//  NPC food search (more intelligent and trusworthy)
-#define NPC_AI_COMBAT			0x00040
-#define NPC_AI_VEND_TIME		0x00080
-#define NPC_AI_LOOTING			0x00100
-#define	NPC_AI_MOVEOBSTACLES	0x00200
-#define NPC_AI_PERSISTENTPATH	0x00400
-#define NPC_AI_THREAT			0x00800
-#define NPC_AI_OLDSPEED			0x01000		// Enable backwards MoveSpeed formula ()
+#define NPC_AI_PATH				0x001	// Enable advanced NPC pathfinding engine (this can significantly increase server CPU usage)
+#define	NPC_AI_FOOD				0x002	// Enable basic NPC food search (objects + grass) when hungry
+#define	NPC_AI_EXTRA			0x004	// Make NPCs human (brain_human) equip/unequip weapons on combat or light sources at night
+#define NPC_AI_ALWAYSINT		0x008	// Make NPC_AI_PATH consider all NPCs as smart as possible to find better paths
+#define NPC_AI_INTFOOD			0x010	// Enable advanced NPC food search (more intelligent and trusworthy) when hungry
+#define NPC_AI_COMBAT			0x040	// Make NPCs cast good spells in his friends while in combat
+#define NPC_AI_LOOTING			0x100	// Make NPCs loot nearby corpses and items on ground
+#define	NPC_AI_MOVEOBSTACLES	0x200	// Make NPCs with CAN=mt_usehands able to move items blocking its way
+#define NPC_AI_PERSISTENTPATH	0x400	// Make NPCs try to follow its target even when it is unreachable instead give up
+#define NPC_AI_THREAT			0x800	// Make NPCs switch targets based on target threat level while in combat
 	int		m_iNpcAi;
 
 	//	Experience system
@@ -948,7 +946,6 @@ public:
 	unsigned int m_iLevelNextAt;
 
 	bool	m_bAutoResDisp;
-	int		m_iAutoPrivFlags;
 
 	char	m_cCommandPrefix;
 

src/sphere.ini
@@ -220,9 +220,6 @@ UseAuthID=1
 // Set account RESDISP automatically based on player client version
 AutoResDisp=1
 
-// Default setting for new accounts specifying default priv level
-//AutoPrivFlags=010
-
 // How often send my hits updates to visible clients (in seconds)
 //HitsUpdateRate=1
 
@@ -498,18 +495,16 @@ SpeechPet=spk_pet
 OverSkillMultiply=2
 
 // NPC AI settings
-// NPC_AI_PATH				00001	NPC pathfinding
-// NPC_AI_FOOD				00002	NPC food search (objects + grass)
-// NPC_AI_EXTRA				00004	NPC magics, etc
-// NPC_AI_ALWAYSINT			00008	Always be as smart as possible with pathfinding
-// NPC_AI_INTFOOD			00010	NPC food search (more intelligent and trusworthy)
-// NPC_AI_COMBAT			00040	Look for friends in combat
-// NPC_AI_VEND_TIME			00080	vendors closing their shops at nighttime
-// NPC_AI_LOOTING			00100	loot corpses an the way
-// NPC_AI_MOVEOBSTACLES		00200	if moveable items block my way, try to move them
-// NPC_AI_PERSISTENTPATH	00400	NPC will try often to find a path with pathfinding
-// NPC_AI_THREAT			00800	Enable the use of the threat variable when finding for target while fighting.
-// NPC_AI_OLDMOVESPEED		01000	Enable backwards SpeedMove formula (55i like) for walk delays.
+// NPC_AI_PATH				00001	Enable advanced NPC pathfinding engine (this can significantly increase server CPU usage)
+// NPC_AI_FOOD				00002	Enable basic NPC food search (objects + grass) when hungry
+// NPC_AI_EXTRA				00004	Make NPCs human (brain_human) equip/unequip weapons on combat or light sources at night
+// NPC_AI_ALWAYSINT			00008	Make NPC_AI_PATH consider all NPCs as smart as possible to find better paths
+// NPC_AI_INTFOOD			00010	Enable advanced NPC food search (more intelligent and trusworthy) when hungry
+// NPC_AI_COMBAT			00040	Make NPCs cast good spells in his friends while in combat
+// NPC_AI_LOOTING			00100	Make NPCs loot nearby items and corpses
+// NPC_AI_MOVEOBSTACLES		00200	Make NPCs with CAN=mt_usehands able to move items blocking its way
+// NPC_AI_PERSISTENTPATH	00400	Make NPCs try to follow its target even when it is unreachable instead give up
+// NPC_AI_THREAT			00800	Make NPCs attack targets that have higher threat level in combat
 //NPCAI=0
 
 ///////////////////////////////////////////////////////////////
@@ -617,8 +612,8 @@ NotoTimeOut=30
 // EF_DynamicBackgroundsave		00000002
 // EF_ItemStacking				00000004 // Enable item stacking feature when drop items on ground
 // EF_ItemStackDrop				00000008 // The item stack will drop when an item got removed from the stack
-// EF_FastWalkPrevention		00000010 // Enable client fastwalk prevention (works better on Linux)(if enabled, it will override old WalkBuffer/WalkRegen checks)
-// EF_Intrinsic_Locals			00000020 // Disables the needing of 'local.', 'tag.', etc. Be aware of not creating variables with the same name of already-existing functions.
+// EF_FastWalkPrevention		00000010 // Use new client fastwalk prevention instead old WalkBuffer/WalkRegen checks. To avoid false-positive results, make sure that your OS/CPU/BIOS have HPET (High Precision Event Timer) feature enabled
+// EF_Intrinsic_Locals			00000020 // Disables the needing of 'local.', 'tag.', etc. Be aware of not creating variables with the same name of already-existing functions
 // EF_Item_Strict_Comparison	00000040 // Don't consider log/board and leather/hide as the same resource type
 // EF_AllowTelnetPacketFilter	00000200 // Enable packet filtering for telnet connections as well
 // EF_Script_Profiler			00000400 // Record all functions/triggers execution time statistics (it can be viewed pressing P on console)

commit 0dd6c069d74ca47145647644bf220d6fd3923629

Fixed account PRIV error caused by previous commit https://github.com/Sphereserver/Source/commit/7a18fdc4cc06147d3cef8d78dd6f77bf22693d5e

src/graysvr/CAccount.cpp
@@ -537,25 +537,20 @@ PLEVEL_TYPE CAccount::GetPrivLevelText( LPCTSTR pszFlags ) // static
 
 CAccount::CAccount( LPCTSTR pszName, bool fGuest )
 {
-	g_Serv.StatInc( SERV_STAT_ACCOUNTS );
+	g_Serv.StatInc(SERV_STAT_ACCOUNTS);
 
-	TCHAR szName[ MAX_ACCOUNT_NAME_SIZE ];
-	if ( !CAccount::NameStrip( szName, pszName ) )
-	{
+	TCHAR szName[MAX_ACCOUNT_NAME_SIZE];
+	if ( !CAccount::NameStrip(szName, pszName) )
 		g_Log.Event(LOGL_ERROR|LOGM_INIT, "Account '%s': BAD name\n", pszName);
-	}
+
 	m_sName = szName;
-
-	if ( !strnicmp(m_sName, "GUEST", 5) || fGuest )
-		SetPrivLevel(PLEVEL_Guest);
-	else
-		SetPrivLevel(PLEVEL_Player);
-
+	SetPrivLevel((!strnicmp(m_sName, "GUEST", 5) || fGuest) ? PLEVEL_Guest : PLEVEL_Player);
+	m_PrivFlags = 0;
 	m_MaxChars = 0;
 	m_Total_Connect_Time = 0;
 	m_Last_Connect_Time = 0;
-	// Add myself to the list.
-	g_Accounts.Account_Add( this );
+
+	g_Accounts.Account_Add(this);
 }
 
 void CAccount::DeleteChars()
@@ -1366,9 +1361,9 @@ void CAccount::r_Write(CScript &s)
 	{
 		s.WriteKey( "PLEVEL", sm_szPrivLevels[ GetPrivLevel() ] );
 	}
-	if ( m_PrivFlags != PRIV_DETAIL )
+	if ( m_PrivFlags )
 	{
-		s.WriteKeyHex( "PRIV", m_PrivFlags &~( PRIV_BLOCKED | PRIV_JAILED ));
+		s.WriteKeyHex( "PRIV", m_PrivFlags & ~(PRIV_BLOCKED|PRIV_JAILED) );
 	}
 	if ( GetResDisp() )
 	{