bb80e5bdd500956b18ef40787d75bec50a545f34
Coruja <coruja@outlook.com>
2019-01-13 20:21:47 -0200

    Updated copyright

--- src/graysvr/resource.h
+++ src/graysvr/resource.h
@@ -13,11 +13,6 @@
 #define IDM_POP_LOG					103
 #define IDC_STAT_STATS				201
 #define IDC_STAT_CLIENTS			202
-#define IDC_O_LOG_LEVEL				250
-#define IDC_O_LOG_CLIENTS			251
-#define IDC_O_LOG_GM_CMDS			252
-#define IDC_O_LOG_GM_PAGES			253
-#define IDC_O_LOG_SPEAK				254
 #define IDM_STATUS					528
 #define IDM_RESTORE					544
 #define IDM_MINIMIZE				560
@@ -25,13 +20,7 @@
 #define IDM_EXIT					582
 #define IDC_ABOUT_VERSION			1117
 #define IDC_ABOUT_COMPILER			1118
-#define IDC_ABOUT_MENASOFT_LINK		1119
-#define IDC_ABOUT_SPHERE_LINK		1120
-#define IDC_SETUP_PORT				4020
-#define IDC_SETUP_SCRIPTS			4021
-#define IDC_SETUP_SAVE				4022
-#define IDC_SETUP_LOG				4023
-#define IDC_SETUP_NAME				4024
+#define IDC_ABOUT_SPHERE_LINK		1119
 #define IDM_RESYNC_PAUSE			32784
 
 // Next default values for new objects
