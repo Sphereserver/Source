Sphereserver/Source

commit 063dc07ddfd86dea2fa2fa470224771f44306ca6

Fixed: Scripts not parsing " " quotes as string correctly

src/common/CScriptObj.cpp
@@ -1992,7 +1992,7 @@ bool CScriptObj::r_Verb(CScript &s, CTextConsole *pSrc)
 		{
 			if ( !pRef )
 				return true;
-			CScript script(pszKey, s.GetArgStr());
+			CScript script(pszKey, s.GetArgRaw());
 			return pRef->r_Verb(script, pSrc);
 		}
 		// else just fall through. as they seem to be setting the pointer !?
@@ -2008,7 +2008,7 @@ bool CScriptObj::r_Verb(CScript &s, CTextConsole *pSrc)
 			if ( !pRef )
 				return false;
 		}
-		CScript script(pszKey, s.GetArgStr());
+		CScript script(pszKey, s.GetArgRaw());
 		return pRef->r_Verb(script, pSrc);
 	}
 
@@ -2517,7 +2517,7 @@ bool CScriptTriggerArgs::r_Verb(CScript &s, CTextConsole *pSrc)
 	int index = -1;
 
 	if ( !strnicmp("FLOAT.", pszKey, 6) )
-		return m_VarsFloat.Insert(pszKey + 6, s.GetArgStr(), true);
+		return m_VarsFloat.Insert(pszKey + 6, s.GetArgRaw(), true);
 	else if ( !strnicmp("LOCAL.", pszKey, 6) )
 	{
 		bool fQuoted = false;
@@ -2548,7 +2548,7 @@ bool CScriptTriggerArgs::r_Verb(CScript &s, CTextConsole *pSrc)
 					if ( !pObj )
 						return false;
 
-					CScript script(pszKey, s.GetArgStr());
+					CScript script(pszKey, s.GetArgRaw());
 					return pObj->r_Verb(script, pSrc);
 				}
 			}
@@ -2582,7 +2582,7 @@ bool CScriptTriggerArgs::r_Verb(CScript &s, CTextConsole *pSrc)
 			m_iN3 = s.GetArgLLVal();
 			return true;
 		case AGC_S:
-			Init(s.GetArgStr());
+			Init(s.GetArgRaw());
 			return true;
 		case AGC_O:
 		{
@@ -2593,7 +2593,7 @@ bool CScriptTriggerArgs::r_Verb(CScript &s, CTextConsole *pSrc)
 				if ( !m_pO1 )
 					return false;
 
-				CScript script(pszTemp, s.GetArgStr());
+				CScript script(pszTemp, s.GetArgRaw());
 				return m_pO1->r_Verb(script, pSrc);
 			}
 			return false;
@@ -2601,8 +2601,8 @@ bool CScriptTriggerArgs::r_Verb(CScript &s, CTextConsole *pSrc)
 		case AGC_TRY:
 		case AGC_TRYSRV:
 		{
-			CScript try_script(s.GetArgStr());
-			if ( r_Verb(try_script, pSrc) )
+			CScript script(s.GetArgRaw());
+			if ( r_Verb(script, pSrc) )
 				return true;
 		}
 		default:

commit 1996a214077cb4decbb35f2fb0c0a8ff43b7304a

Fixed some script parsing issues caused by commit https://github.com/Sphereserver/Source/commit/063dc07ddfd86dea2fa2fa470224771f44306ca6

src/common/CScriptObj.cpp
@@ -1324,9 +1324,7 @@ bool CScriptObj::r_LoadVal(CScript &s)
 			{
 				if ( !strcmpi(pszKey, g_Exp.sm_szMsgNames[i]) )
 				{
-					bool fQuoted = false;
-					TCHAR *pszArgs = s.GetArgStr(&fQuoted);
-					strncpy(g_Exp.sm_szMessages[i], pszArgs, EXPRESSION_MAX_KEY_LEN - 1);
+					strncpy(g_Exp.sm_szMessages[i], s.GetArgStr(), EXPRESSION_MAX_KEY_LEN - 1);
 					return true;
 				}
 			}
@@ -1931,7 +1929,7 @@ bool CScriptObj::r_Verb(CScript &s, CTextConsole *pSrc)
 		{
 			if ( !pRef )
 				return true;
-			CScript script(pszKey, s.GetArgRaw());
+			CScript script(pszKey, s.GetArgStr());
 			return pRef->r_Verb(script, pSrc);
 		}
 		// else just fall through. as they seem to be setting the pointer !?
@@ -2724,7 +2722,7 @@ bool CFileObj::r_LoadVal(CScript &s)
 			return true;
 		}
 		else
-			g_Log.Event(LOGL_ERROR, "FILE (%s): Cannot set mode after file opening\n", static_cast<LPCTSTR>(m_pFile->GetFilePath()));
+			g_Log.Event(LOGL_ERROR, "Can't change access mode of open file '%s', it must be closed first\n", static_cast<LPCTSTR>(m_pFile->GetFilePath()));
 		return false;
 	}
 
@@ -2737,7 +2735,7 @@ bool CFileObj::r_LoadVal(CScript &s)
 		{
 			if ( !m_pFile->IsFileOpen() )
 			{
-				g_Log.Event(LOGL_ERROR, "FILE: Cannot write content. Open the file first\n");
+				g_Log.Event(LOGL_ERROR, "Failed to write on file '%s', it must be opened first\n", static_cast<LPCTSTR>(m_pFile->GetFilePath()));
 				return false;
 			}
 
@@ -2767,7 +2765,7 @@ bool CFileObj::r_LoadVal(CScript &s)
 
 			if ( !fSuccess )
 			{
-				g_Log.Event(LOGL_ERROR, "FILE: Failed writing to \"%s\"\n", static_cast<LPCTSTR>(m_pFile->GetFilePath()));
+				g_Log.Event(LOGL_ERROR, "Failed to write on file '%s'\n", static_cast<LPCTSTR>(m_pFile->GetFilePath()));
 				return false;
 			}
 			break;
@@ -2877,7 +2875,7 @@ bool CFileObj::r_WriteVal(LPCTSTR pszKey, CGString &sVal, CTextConsole *pSrc)
 
 			if ( m_pFile->IsFileOpen() )
 			{
-				g_Log.Event(LOGL_ERROR, "FILE: Cannot open file (%s). First close \"%s\"\n", pszFilename, static_cast<LPCTSTR>(m_pFile->GetFilePath()));
+				g_Log.Event(LOGL_ERROR, "Failed to open file '%s', another file is already open\n", pszFilename);
 				return false;
 			}
 
@@ -2903,14 +2901,14 @@ bool CFileObj::r_WriteVal(LPCTSTR pszKey, CGString &sVal, CTextConsole *pSrc)
 
 			if ( (m_pFile->GetPosition() + iRead > m_pFile->GetLength()) || m_pFile->IsEOF() )
 			{
-				g_Log.Event(LOGL_ERROR, "FILE: Failed reading %" FMTSIZE_T " byte from \"%s\". Too near to EOF\n", iRead, static_cast<LPCTSTR>(m_pFile->GetFilePath()));
+				g_Log.Event(LOGL_ERROR, "Failed to read %" FMTSIZE_T " bytes from file '%s' (EOF reached)\n", iRead, static_cast<LPCTSTR>(m_pFile->GetFilePath()));
 				return false;
 			}
 
 			TCHAR *pszBuffer = GetReadBuffer(true);
 			if ( iRead != m_pFile->Read(pszBuffer, iRead) )
 			{
-				g_Log.Event(LOGL_ERROR, "FILE: Failed reading %" FMTSIZE_T " byte from \"%s\"\n", iRead, static_cast<LPCTSTR>(m_pFile->GetFilePath()));
+				g_Log.Event(LOGL_ERROR, "Failed to read %" FMTSIZE_T " bytes from file '%s'\n", iRead, static_cast<LPCTSTR>(m_pFile->GetFilePath()));
 				return false;
 			}
 

src/graysvr/CResource.cpp
@@ -3598,24 +3598,23 @@ bool CResource::Load(bool fResync)
 	return true;
 }
 
-LPCTSTR CResource::GetDefaultMsg(long lKeyNum)
+LPCTSTR CResource::GetDefaultMsg(int iKeyNum)
 {
 	ADDTOCALLSTACK("CResource::GetDefaultMsg");
-	if ( (lKeyNum < 0) || (lKeyNum >= DEFMSG_QTY) )
+	if ( (iKeyNum < 0) || (iKeyNum >= DEFMSG_QTY) )
 		return "";
 
-	return g_Exp.sm_szMessages[lKeyNum];
+	return g_Exp.sm_szMessages[iKeyNum];
 }
 
 LPCTSTR CResource::GetDefaultMsg(LPCTSTR pszKey)
 {
 	ADDTOCALLSTACK("CResource::GetDefaultMsg");
-	for ( size_t i = 0; i < DEFMSG_QTY; ++i )
+	for ( int i = 0; i < DEFMSG_QTY; ++i )
 	{
 		if ( !strcmpi(pszKey, g_Exp.sm_szMsgNames[i]) )
 			return g_Exp.sm_szMessages[i];
 	}
-	g_Log.EventError("Defmessage \"%s\" not found\n", pszKey);
 	return "";
 }
 

src/graysvr/CResource.h
@@ -1127,7 +1127,7 @@ public:
 
 	#define SysMessageDefault(pszKey)		SysMessage(g_Cfg.GetDefaultMsg(pszKey))
 	LPCTSTR GetDefaultMsg(LPCTSTR pszKey);
-	LPCTSTR GetDefaultMsg(long lKeyNum);
+	LPCTSTR GetDefaultMsg(int iKeyNum);
 
 	typedef std::map<DWORD, DWORD> KRGumpsMap;
 	KRGumpsMap m_mapKRGumps;