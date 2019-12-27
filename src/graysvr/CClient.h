Sphereserver/Source

commit bf8f2e3fcc70061577bc9b4bacf74d95ab02278e

Misc optimizations changing some functions from 'bool' to 'void'

src/graysvr/CClient.h
@@ -510,23 +510,23 @@ private:
 	void Announce(bool fArrive) const;
 
 	// GM stuff
-	bool OnTarg_Obj_Set(CObjBase *pObj);
-	bool OnTarg_Obj_Info(CObjBase *pObj, const CPointMap &pt, ITEMID_TYPE id);
-	bool OnTarg_Obj_Function(CObjBase *pObj, const CPointMap &pt, ITEMID_TYPE id);
+	void OnTarg_Obj_Set(CObjBase *pObj);
+	void OnTarg_Obj_Info(CObjBase *pObj, const CPointMap &pt, ITEMID_TYPE id);
+	void OnTarg_Obj_Function(CObjBase *pObj, const CPointMap &pt, ITEMID_TYPE id);
 
-	bool OnTarg_UnExtract(CObjBase *pObj, const CPointMap &pt);
-	bool OnTarg_Stone_Recruit(CChar *pChar, bool fFull = false);
-	bool OnTarg_Char_Add(CObjBase *pObj, const CPointMap &pt);
-	bool OnTarg_Item_Add(CObjBase *pObj, CPointMap &pt);
-	bool OnTarg_Item_Link(CObjBase *pObj);
-	bool OnTarg_Tile(CObjBase *pObj, const CPointMap &pt);
+	void OnTarg_UnExtract(CObjBase *pObj, const CPointMap &pt);
+	void OnTarg_Stone_Recruit(CChar *pChar, bool fFull = false);
+	void OnTarg_Char_Add(CObjBase *pObj, const CPointMap &pt);
+	void OnTarg_Item_Add(CObjBase *pObj, CPointMap &pt);
+	void OnTarg_Item_Link(CObjBase *pObj);
+	void OnTarg_Tile(CObjBase *pObj, const CPointMap &pt);
 
 	// Player stuff
 	bool OnTarg_Use_Deed(CItem *pDeed, CPointMap &pt);
 	bool OnTarg_Use_Item(CObjBase *pObjTarg, CPointMap &pt, ITEMID_TYPE id);
-	bool OnTarg_Party_Add(CChar *pChar);
-	bool OnTarg_Party_Remove(CChar *pChar);
-	bool OnTarg_GlobalChat_Add(CChar *pChar);
+	void OnTarg_Party_Add(CChar *pChar);
+	void OnTarg_Party_Remove(CChar *pChar);
+	void OnTarg_GlobalChat_Add(CChar *pChar);
 	CItem *OnTarg_Use_Multi(const CItemBase *pItemDef, CPointMap &pt, DWORD dwAttr, HUE_TYPE wHue);
 
 	int OnSkill_AnimalLore(CGrayUID uid, bool fTest);
@@ -537,19 +537,19 @@ private:
 	int OnSkill_ItemID(CGrayUID uid, bool fTest);
 	int OnSkill_TasteID(CGrayUID uid, bool fTest);
 
-	bool OnTarg_Pet_Command(CObjBase *pObj, const CPointMap &pt);
-	bool OnTarg_Pet_Stable(CChar *pCharPet);
+	void OnTarg_Pet_Command(CObjBase *pObj, const CPointMap &pt);
+	void OnTarg_Pet_Stable(CChar *pCharPet);
 
 	// Commands from client
 	void Event_Talk_Common(TCHAR *szText);	// PC speech
 	bool Event_Command(LPCTSTR pszCommand, TALKMODE_TYPE mode = TALKMODE_SYSTEM);	// client entered a '/' command like /ADD
 
 public:
-	bool OnTarg_Skill(CObjBase *pObj);
-	bool OnTarg_Skill_Provoke(CObjBase *pObj);
-	bool OnTarg_Skill_Poison(CObjBase *pObj);
-	bool OnTarg_Skill_Herd_Dest(CObjBase *pObj, const CPointMap &pt);
-	bool OnTarg_Skill_Magery(CObjBase *pObj, const CPointMap &pt);
+	void OnTarg_Skill(CObjBase *pObj);
+	void OnTarg_Skill_Provoke(CObjBase *pObj);
+	void OnTarg_Skill_Poison(CObjBase *pObj);
+	void OnTarg_Skill_Herd_Dest(CObjBase *pObj, const CPointMap &pt);
+	void OnTarg_Skill_Magery(CObjBase *pObj, const CPointMap &pt);
 
 	void GetAdjustedCharID(const CChar *pChar, CREID_TYPE &id, HUE_TYPE &wHue) const;
 	void GetAdjustedItemID(const CChar *pChar, const CItem *pItem, ITEMID_TYPE &id, HUE_TYPE &wHue) const;
@@ -591,7 +591,7 @@ public:
 	TRIGRET_TYPE Menu_OnSelect(RESOURCE_ID_BASE rid, int iSelect, CObjBase *pObj);
 	TRIGRET_TYPE Dialog_OnButton(RESOURCE_ID_BASE rid, DWORD dwButtonID, CObjBase *pObj, CDialogResponseArgs *pArgs);
 
-	bool Login_Relay(WORD wRelay);		// relay player to a certain IP
+	void Login_Relay(WORD wRelay);		// relay player to a certain IP
 	BYTE Login_ServerList(LPCTSTR pszAccount, LPCTSTR pszPassword);		// initial login (Login on "loginserver", new format)
 
 	BYTE Setup_FillCharList(Packet *pPacket);	// write character list to packet
@@ -634,14 +634,14 @@ public:
 	// Low level message traffic
 	static size_t xCompress(BYTE *pOutput, const BYTE *pInput, size_t iInputLen);
 
-	bool xProcessClientSetup(CEvent *pEvent, size_t iLen);
+	void xProcessClientSetup(CEvent *pEvent, size_t iLen);
 	bool xPacketFilter(const BYTE *pData, size_t iLen = 0);
 	bool xOutPacketFilter(const BYTE *pData, size_t iLen = 0);
 	bool xCanEncLogin(bool fCheckCliver = false);	// login crypt check
 
 	// Low level push world data to the client
-	bool addRelay(const CServerDef *pServ);
-	bool addLoginErr(BYTE bCode);
+	void addRelay(const CServerDef *pServ);
+	void addLoginErr(BYTE bCode);
 
 	#define SF_UPDATE_HITS		0x1
 	#define SF_UPDATE_MANA		0x2
@@ -668,7 +668,7 @@ public:
 	void UpdateStats();
 	void UpdateFeatureFlags();
 	void UpdateCharacterListFlags();
-	bool addDeleteErr(BYTE bCode);
+	void addDeleteErr(BYTE bCode);
 	void addSeason(SEASON_TYPE season);
 	void addTime(bool fCurrent = false);
 	void closeUIWindow(DWORD dwWindowType, const CObjBase *pObj);
@@ -759,7 +759,7 @@ public:
 	void addSkillWindow(SKILL_TYPE skill, bool fFromInfo = false);
 	void addSpecialMoveClear();
 	void addBulletinBoard(const CItemContainer *pBoard);
-	bool addBBoardMessage(const CItemContainer *pBoard, BULLETINBOARD_TYPE action, CGrayUID uidMsg);
+	void addBBoardMessage(const CItemContainer *pBoard, BULLETINBOARD_TYPE action, CGrayUID uidMsg);
 
 	void addToolTip(const CObjBase *pObj, LPCTSTR pszText);
 	void addDrawMap(CItemMap *pMap);
@@ -771,7 +771,7 @@ public:
 	void addItemMenu(CLIMODE_TYPE mode, const CMenuItem *item, size_t iCount, CObjBase *pObj = NULL);
 	void addGumpDialog(CLIMODE_TYPE mode, const CGString *psControls, size_t iControls, const CGString *psText, size_t iTexts, DWORD x, DWORD y, CObjBase *pObj = NULL, DWORD rid = 0);
 
-	bool addGumpDialogProps(CObjBase *pObj);
+	void addGumpDialogProps(CObjBase *pObj);
 
 	void addLoginComplete();
 	void addChatSystemMessage(CHATMSG_TYPE type, LPCTSTR pszName1 = NULL, LPCTSTR pszName2 = NULL, CLanguageID lang = 0);
@@ -859,11 +859,10 @@ public:
 			return m_pAccount->GetResDisp();
 		return BYTE_MAX;
 	}
-	bool SetResDisp(BYTE bResDisp)
+	void SetResDisp(BYTE bResDisp)
 	{
 		if ( m_pAccount )
-			return m_pAccount->SetResDisp(bResDisp);
-		return false;
+			m_pAccount->SetResDisp(bResDisp);
 	}
 
 	void SetScreenSize(WORD x, WORD y)
@@ -893,7 +892,7 @@ public:
 	bool CanSee(const CObjBaseTemplate *pObj) const;
 	bool CanHear(const CObjBaseTemplate *pSrc, TALKMODE_TYPE mode) const;
 
-	bool Dialog_Setup(CLIMODE_TYPE mode, RESOURCE_ID_BASE rid, int iPage, CObjBase *pObj, LPCTSTR pszArgs = "");
+	void Dialog_Setup(CLIMODE_TYPE mode, RESOURCE_ID_BASE rid, int iPage, CObjBase *pObj, LPCTSTR pszArgs = "");
 	bool Dialog_Close(CObjBase *pObj, DWORD rid, DWORD dwButtonID);
 	void Menu_Setup(RESOURCE_ID_BASE rid, CObjBase *pObj = NULL);
 

src/graysvr/CClientMsg.cpp
@@ -397,14 +397,10 @@ void CClient::removeBuff(const BUFF_ICONS IconID)
 	new PacketBuff(this, IconID);
 }
 
-bool CClient::addDeleteErr(BYTE bCode)
+void CClient::addDeleteErr(BYTE bCode)
 {
 	ADDTOCALLSTACK("CClient::addDeleteErr");
-	if ( bCode == PacketDeleteError::Success )
-		return true;
-
 	new PacketDeleteError(this, static_cast<PacketDeleteError::Reason>(bCode));
-	return false;
 }
 
 void CClient::addTime(bool fCurrent)
@@ -2326,24 +2322,21 @@ void CClient::addBulletinBoard(const CItemContainer *pBoard)
 	addContents(pBoard);
 }
 
-bool CClient::addBBoardMessage(const CItemContainer *pBoard, BULLETINBOARD_TYPE action, CGrayUID uidMsg)
+void CClient::addBBoardMessage(const CItemContainer *pBoard, BULLETINBOARD_TYPE action, CGrayUID uidMsg)
 {
 	ADDTOCALLSTACK("CClient::addBBoardMessage");
-	ASSERT(pBoard);
+	if ( !pBoard )
+		return;
 
 	CItemMessage *pMsgItem = dynamic_cast<CItemMessage *>(uidMsg.ItemFind());
 	if ( !pBoard->IsItemInside(pMsgItem) )
-		return false;
+		return;
 
 	// Check if author is properly linked
 	if ( !pMsgItem->m_sAuthor.IsEmpty() && !pMsgItem->m_uidLink.CharFind() )
-	{
-		pMsgItem->Delete();
-		return false;
-	}
+		return pMsgItem->Delete();
 
 	new PacketBulletinBoard(this, action, pBoard, pMsgItem);
-	return true;
 }
 
 void CClient::addLoginComplete()

src/graysvr/CAccount.cpp
@@ -621,31 +621,32 @@ PLEVEL_TYPE CAccount::GetPrivLevelText(LPCTSTR pszFlags)	// static
 	return static_cast<PLEVEL_TYPE>(iPlevel);
 }
 
-bool CAccount::SetAutoResDisp(CClient *pClient)
+void CAccount::SetAutoResDisp(CClient *pClient)
 {
 	ADDTOCALLSTACK("CAccount::SetAutoResDisp");
 	// Set account RESDISP automatically based on player's client version
 	if ( !pClient )
-		return false;
+		return;
 
+	BYTE bResDisp = RDS_NONE;
 	if ( pClient->m_NetState->isClientVersion(MINCLIVER_TOL) )
-		return SetResDisp(RDS_TOL);
+		bResDisp = RDS_TOL;
 	else if ( pClient->m_NetState->isClientVersion(MINCLIVER_HS) )
-		return SetResDisp(RDS_HS);
+		bResDisp = RDS_HS;
 	else if ( pClient->m_NetState->isClientVersion(MINCLIVER_SA) )
-		return SetResDisp(RDS_SA);
+		bResDisp = RDS_SA;
 	else if ( pClient->m_NetState->isClientVersion(MINCLIVER_ML) )
-		return SetResDisp(RDS_ML);
+		bResDisp = RDS_ML;
 	else if ( pClient->m_NetState->isClientVersion(MINCLIVER_SE) )
-		return SetResDisp(RDS_SE);
+		bResDisp = RDS_SE;
 	else if ( pClient->m_NetState->isClientVersion(MINCLIVER_AOS) )
-		return SetResDisp(RDS_AOS);
+		bResDisp = RDS_AOS;
 	else if ( pClient->m_NetState->isClientVersion(MINCLIVER_LBR) )
-		return SetResDisp(RDS_LBR);
+		bResDisp = RDS_LBR;
 	else if ( pClient->m_NetState->isClientVersion(MINCLIVER_T2A) )
-		return SetResDisp(RDS_T2A);
-	else
-		return SetResDisp(RDS_NONE);
+		bResDisp = RDS_T2A;
+
+	SetResDisp(bResDisp);
 }
 
 void CAccount::OnLogin(CClient *pClient)

src/graysvr/CAccount.h
@@ -186,12 +186,11 @@ public:
 	{
 		return m_ResDisp;
 	}
-	bool SetResDisp(BYTE bResDisp)
+	void SetResDisp(BYTE bResDisp)
 	{
 		m_ResDisp = bResDisp;
-		return true;
 	}
-	bool SetAutoResDisp(CClient *pClient);
+	void SetAutoResDisp(CClient *pClient);
 
 	void OnLogin(CClient *pClient);
 	void OnLogout(CClient *pClient, bool fWasChar = false);

src/graysvr/CClientDialog.cpp
@@ -593,23 +593,22 @@ bool CDialogDef::GumpSetup( int iPage, CClient * pClient, CObjBase * pObjSrc, LP
 	return true;
 }
 
-
-bool CClient::Dialog_Setup( CLIMODE_TYPE mode, RESOURCE_ID_BASE rid, int iPage, CObjBase * pObj, LPCTSTR Arguments )
+void CClient::Dialog_Setup( CLIMODE_TYPE mode, RESOURCE_ID_BASE rid, int iPage, CObjBase * pObj, LPCTSTR Arguments )
 {
 	ADDTOCALLSTACK("CClient::Dialog_Setup");
 	if ( !pObj )
-		return false;
+		return;
 
 	CResourceDef *pRes = g_Cfg.ResourceGetDef(rid);
 	CDialogDef *pDlg = dynamic_cast<CDialogDef *>(pRes);
 	if ( !pDlg )
 	{
 		DEBUG_ERR(("Invalid RES_DIALOG\n"));
-		return false;
+		return;
 	}
 
 	if ( !pDlg->GumpSetup(iPage, this, pObj, Arguments) )
-		return false;
+		return;
 
 	// Now pack it up to send,
 	// m_tmGumpDialog.m_ResourceID = rid;
@@ -624,7 +623,6 @@ bool CClient::Dialog_Setup( CLIMODE_TYPE mode, RESOURCE_ID_BASE rid, int iPage, 
 	}
 
 	addGumpDialog(mode, pDlg->m_sControls, pDlg->m_iControls, pDlg->m_sText, pDlg->m_iTexts, pDlg->m_x, pDlg->m_y, pObj, context);
-	return true;
 }
 
 
@@ -672,20 +670,19 @@ void CClient::addGumpDialog(CLIMODE_TYPE mode, const CGString *psControls, size_
 		++m_mapOpenedGumps[context];
 }
 
-bool CClient::addGumpDialogProps(CObjBase *pObj)
+void CClient::addGumpDialogProps(CObjBase *pObj)
 {
 	ADDTOCALLSTACK("CClient::addGumpDialogProps");
 	// Open object properties dialog
 	if ( !pObj )
-		return false;
+		return;
 
 	RESOURCE_ID rid = g_Cfg.ResourceGetIDType(RES_DIALOG, pObj->IsItem() ? "d_ITEMPROP1" : "d_CHARPROP1");
 	if ( !rid.IsValidUID() )
-		return false;
+		return;
 
 	m_Prop_UID = m_Targ_UID = pObj->GetUID();
 	Dialog_Setup(CLIMODE_DIALOG, rid, 0, pObj);
-	return true;
 }
 
 TRIGRET_TYPE CClient::Dialog_OnButton(RESOURCE_ID_BASE rid, DWORD dwButtonID, CObjBase *pObj, CDialogResponseArgs *pArgs)

src/graysvr/CClientLog.cpp
@@ -32,11 +32,11 @@ void CClient::SetConnectType(CONNECT_TYPE iType)
 ///////////////////////////////////////////////////////////
 // Push world display data to this client only
 
-bool CClient::addLoginErr(BYTE bCode)
+void CClient::addLoginErr(BYTE bCode)
 {
 	ADDTOCALLSTACK("CClient::addLoginErr");
 	if ( bCode == PacketLoginError::Success )
-		return true;
+		return;
 
 	// Console message to display for each login error code
 	static const LPCTSTR sm_szLoginError[] =
@@ -72,7 +72,6 @@ bool CClient::addLoginErr(BYTE bCode)
 	}
 
 	m_NetState->markReadClosed();
-	return false;
 }
 
 void CClient::addSysMessage(LPCTSTR pszMsg)
@@ -106,12 +105,12 @@ void CClient::addWebLaunch(LPCTSTR pszURL)
 ///////////////////////////////////////////////////////////
 // Login server
 
-bool CClient::addRelay(const CServerDef *pServ)
+void CClient::addRelay(const CServerDef *pServ)
 {
 	ADDTOCALLSTACK("CClient::addRelay");
 	// Tell the client to play on this server
 	if ( !pServ )
-		return false;
+		return;
 
 	EXC_TRY("addRelay");
 	CSocketAddressIP ipAddr = pServ->m_ip;
@@ -146,16 +145,15 @@ bool CClient::addRelay(const CServerDef *pServ)
 	new PacketServerRelay(this, dwAddr, pServ->m_ip.GetPort(), dwCustomerId);
 
 	m_Targ_Mode = CLIMODE_SETUP_RELAY;
-	return true;
+	return;
 	EXC_CATCH;
 
 	EXC_DEBUG_START;
 	g_Log.EventDebug("account '%s'\n", m_pAccount ? m_pAccount->GetName() : "");
 	EXC_DEBUG_END;
-	return false;
 }
 
-bool CClient::Login_Relay(WORD wRelay)
+void CClient::Login_Relay(WORD wRelay)
 {
 	ADDTOCALLSTACK("CClient::Login_Relay");
 	// Client wants to be relayed to another server (PACKET_ServerSelect)
@@ -178,11 +176,11 @@ bool CClient::Login_Relay(WORD wRelay)
 		if ( !pServ )
 		{
 			DEBUG_ERR(("%lx:Login_Relay Bad index %hu\n", GetSocketID(), wRelay));
-			return false;
+			return;
 		}
 	}
 
-	return addRelay(pServ);
+	addRelay(pServ);
 }
 
 BYTE CClient::Login_ServerList(LPCTSTR pszAccount, LPCTSTR pszPassword)
@@ -593,7 +591,7 @@ bool CClient::OnRxWebPageRequest(BYTE *pRequest, size_t iLen)
 	return false;
 }
 
-bool CClient::xProcessClientSetup(CEvent *pEvent, size_t iLen)
+void CClient::xProcessClientSetup(CEvent *pEvent, size_t iLen)
 {
 	ADDTOCALLSTACK("CClient::xProcessClientSetup");
 	// If this is a login, try to process the data and get client version
@@ -615,22 +613,15 @@ bool CClient::xProcessClientSetup(CEvent *pEvent, size_t iLen)
 		xRecordPacketData(this, (const BYTE *)pEvent, iLen, "client->server");
 #endif
 		DEBUG_MSG(("%lx:Odd login message length %" FMTSIZE_T "\n", GetSocketID(), iLen));
-		addLoginErr(PacketLoginError::UnkCrypt);
-		return false;
+		return addLoginErr(PacketLoginError::UnkCrypt);
 	}
 
 	SetConnectType(m_Crypt.GetConnectType());
 
 	if ( !xCanEncLogin() )
-	{
-		addLoginErr((m_Crypt.GetEncryptionType() == ENC_NONE) ? static_cast<BYTE>(PacketLoginError::BlockNoCrypt) : static_cast<BYTE>(PacketLoginError::BlockCrypt));
-		return false;
-	}
+		return addLoginErr((m_Crypt.GetEncryptionType() == ENC_NONE) ? static_cast<BYTE>(PacketLoginError::BlockNoCrypt) : static_cast<BYTE>(PacketLoginError::BlockCrypt));
 	else if ( (m_Crypt.GetConnectType() == CONNECT_LOGIN) && !xCanEncLogin(true) )
-	{
-		addLoginErr(PacketLoginError::BadClientVer);
-		return false;
-	}
+		return addLoginErr(PacketLoginError::BadClientVer);
 
 	m_Crypt.Decrypt(pEvent->m_Raw, evInputBuffer.m_Raw, iLen);
 	BYTE bErr = PacketLoginError::UnkCrypt;
@@ -640,7 +631,7 @@ bool CClient::xProcessClientSetup(CEvent *pEvent, size_t iLen)
 		case PACKET_ServersReq:
 		{
 			if ( iLen < sizeof(pEvent->ServersReq) )
-				return false;
+				return;
 
 			bErr = Login_ServerList(pEvent->ServersReq.m_acctname, pEvent->ServersReq.m_acctpass);
 			if ( bErr == PacketLoginError::Success )
@@ -671,7 +662,7 @@ bool CClient::xProcessClientSetup(CEvent *pEvent, size_t iLen)
 		case PACKET_CharListReq:
 		{
 			if ( iLen < sizeof(pEvent->CharListReq) )
-				return false;
+				return;
 
 			TCHAR szAccount[MAX_ACCOUNT_NAME_SIZE + 3];
 			Str_GetBare(szAccount, pEvent->CharListReq.m_acctname, sizeof(szAccount) - 1);
@@ -741,8 +732,6 @@ bool CClient::xProcessClientSetup(CEvent *pEvent, size_t iLen)
 
 	if ( bErr != PacketLoginError::Success )
 		addLoginErr(bErr);
-
-	return (bErr == PacketLoginError::Success);
 }
 
 bool CClient::xCanEncLogin(bool fCheckCliver)

src/graysvr/CObjBase.cpp
@@ -2188,7 +2188,8 @@ bool CObjBase::r_Verb(CScript &s, CTextConsole *pSrc)
 			EXC_SET("INFO");
 			if ( !pClientSrc )
 				return false;
-			return pClientSrc->addGumpDialogProps(this);
+			pClientSrc->addGumpDialogProps(this);
+			break;
 		case OV_REMOVE:
 			EXC_SET("REMOVE");
 			Delete();	// remove this object now

commit c9ac0efbb8a6a7ba49eccd8e5a4bfc352fd52c16

Fixed missing 'return' caused by commit https://github.com/Sphereserver/Source/commit/bf8f2e3fcc70061577bc9b4bacf74d95ab02278e

src/graysvr/CClient.h
@@ -689,8 +689,8 @@ public:
 	bool addContainerSetup(const CItemContainer *pContainer);
 
 	void addPlayerStart(CChar *pChar);
-	void addPlayerSee(const CPointMap &ptOld, bool fIgnoreSelfRegion = false);
-	void addPlayerView(const CPointMap &ptOld, bool fFull = true);
+	void addPlayerSee(const CPointMap ptOld, bool fIgnoreSelfRegion = false);
+	void addPlayerView(const CPointMap ptOld, bool fFull = true);
 	void addPlayerWarMode();
 
 	void addCharMove(const CChar *pChar);

src/graysvr/CClientMsg.cpp
@@ -1370,17 +1370,14 @@ void CClient::addPlayerStart(CChar *pChar)
 		m_pChar->ClientAttach(this);
 	}
 	ASSERT(m_pChar->m_pPlayer);
-	ASSERT(m_pAccount);
+
+	CItem *pLingerMemory = m_pChar->LayerFind(LAYER_FLAG_ClientLinger);
+	if ( pLingerMemory )
+		pLingerMemory->Delete();
 
 	CPointMap pt = m_pChar->GetTopPoint();
 	CSector *pSector = pt.GetSector();
-
-	CItem *pItemChange = m_pChar->LayerFind(LAYER_FLAG_ClientLinger);
-	if ( pItemChange )
-		pItemChange->Delete();
-
-	ClearTargMode();	// clear death menu mode. etc. ready to walk about. cancel any previous modes
-	m_Env.SetInvalid();
+	ClearTargMode();
 
 	new PacketPlayerStart(this);
 	addMapDiff();
@@ -1800,7 +1797,7 @@ void CClient::addSpecialMoveClear()
 	new PacketSpecialMoveClear(this);
 }
 
-void CClient::addPlayerSee(const CPointMap &ptOld, bool fIgnoreSelfRegion)
+void CClient::addPlayerSee(const CPointMap ptOld, bool fIgnoreSelfRegion)
 {
 	ADDTOCALLSTACK("CClient::addPlayerSee");
 	// Adjust to my new location. What do I see here?
@@ -1885,7 +1882,7 @@ void CClient::addPlayerSee(const CPointMap &ptOld, bool fIgnoreSelfRegion)
 	}
 }
 
-void CClient::addPlayerView(const CPointMap &ptOld, bool fFull)
+void CClient::addPlayerView(const CPointMap ptOld, bool fFull)
 {
 	ADDTOCALLSTACK("CClient::addPlayerView");
 	// I moved = Change my point of view. Teleport etc..

src/graysvr/CClientTarg.cpp
@@ -1165,6 +1165,7 @@ void CClient::OnTarg_Skill(CObjBase *pObj)
 		{
 			m_pChar->m_Act_Targ = m_Targ_UID;
 			m_pChar->Skill_Start(m_tmSkillTarg.m_Skill);
+			return;
 		}
 		case SKILL_PROVOCATION:
 		{

src/graysvr/CContain.cpp
@@ -823,7 +823,6 @@ CPointMap CItemContainer::GetRandContainerLoc() const
 		int iMinY = (pItemDef->m_ttContainer.m_dwMinXY & 0xFFFF);
 		int iMaxX = (pItemDef->m_ttContainer.m_dwMaxXY & 0xFFFF0000) >> 16;
 		int iMaxY = (pItemDef->m_ttContainer.m_dwMaxXY & 0xFFFF);
-		DEBUG_WARN(("Custom container gump id %d for item 0%x\n", gump, GetDispID()));
 		return CPointMap(static_cast<WORD>(Calc_GetRandVal(iMinX, iMaxX)), static_cast<WORD>(Calc_GetRandVal(iMinY, iMaxY)));
 	}
 
@@ -927,7 +926,7 @@ CPointMap CItemContainer::GetRandContainerLoc() const
 		return CPointMap(static_cast<WORD>(Calc_GetRandVal(sm_ContainerRect[i].m_minX, sm_ContainerRect[i].m_maxX)), static_cast<WORD>(Calc_GetRandVal(sm_ContainerRect[i].m_minY, sm_ContainerRect[i].m_maxY)));
 	}
 
-	DEBUG_WARN(("Unknown container gump id %d for item 0%x\n", gump, GetDispID()));
+	DEBUG_WARN(("Can't get rect of unknown container gump TDATA2=0%hx for item '%s'\n", gump, pItemDef->GetResourceName()));
 	return CPointMap(static_cast<WORD>(Calc_GetRandVal(0, 200)), static_cast<WORD>(Calc_GetRandVal(0, 200)));
 }
 