413e9c5bd4179f65508f0fffc395d1987aa80ba7
Coruja <coruja@outlook.com>
2020-03-27 19:11:27 -0300

    Changed: Updated internal Libev libs v4.31 to v4.33

--- src/common/libev/wrapper_ev.c
+++ src/common/libev/wrapper_ev.c
@@ -3,22 +3,11 @@
 // libev produces many warnings which isn't really appropriate for us to
 // address since it is 3rd party code that could be overwritten at any time
 // with a new version
-#pragma warning(disable:4100 4101 4127 4189 4245 4706 4996 4068)
 #pragma GCC diagnostic ignored "-Wcomment"
-#pragma GCC diagnostic ignored "-Wold-style-declaration"
 #pragma GCC diagnostic ignored "-Wparentheses"
-#pragma GCC diagnostic ignored "-Wsign-compare"
-#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 #pragma GCC diagnostic ignored "-Wunused-value"
-#pragma GCC diagnostic ignored "-Wunused-variable"
-
-#if (__GNUC__ >= 6)
-	#pragma GCC diagnostic ignored "-Wmisleading-indentation"
-	#pragma GCC diagnostic ignored "-Wreturn-type"
-#endif
-#if (__GNUC__ >= 7)
-	#pragma GCC diagnostic ignored "-Wdangling-else"
-#endif
 
 #ifdef __MINGW32__
 #include <time.h>
