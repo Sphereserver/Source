367f1be296b5c6a777d680487c38de4957d69a96
Coruja <coruja747@users.noreply.github.com>
2026-07-24 16:09:36 -0300

    Updated internal MySQL libs v6.1.10 (MySQL client 5.7.16) to v8.0.46
    > [!IMPORTANT]
    > It's HIGHLY recommended to update MySQL client library to prevent errors when using MySQL
    > - Windows: replace file `libmysql.dll` with the version included in the downloaded zip
    > - Linux:
    >   - Debian-based: update package `libmysqlclient20` to `libmysqlclient21` using `sudo apt-get install libmysqlclient21`
    >   - RHEL-based: switch MySQL module stream to v8.0 using `sudo dnf module switch-to mysql:8.0`

--- README.md
+++ README.md
@@ -4,143 +4,93 @@ Ultima Online game server, developed in C++
 
 ## Download
 
-* [GitHub Releases](../../releases)
-* [SphereServer website](https://forum.spherecommunity.net/sshare.php?srt=4)
+- [GitHub Releases](../../releases)
+- [SphereServer Forum](https://forum.spherecommunity.net/sshare.php?srt=4)
+
+> [!NOTE]
+> Recommended OS for pre-compiled builds:
+> - Windows: Windows 10 or later, Windows Server 2016 or later
+> - Linux: Debian 12 or later, Ubuntu 22.04 or later (requires MySQL client library installed)
 
 > [!IMPORTANT]
-> For maximum security, only download builds from official sources and verify the file SHA-256 checksum using the system terminal:
-> * Windows (PowerShell): `Get-FileHash "path/to/file"`
-> * Linux: `sha256sum "path/to/file"`
+> To ensure file integrity, only download builds from official sources and verify the SHA-256 checksum using the system terminal:
+> - Windows: `certutil -hashfile "path/to/file" SHA256`
+> - Linux: `sha256sum "path/to/file"`
 
 ## Building on Windows (Visual Studio)
 
-* Open the project file `SphereSvr.vcxproj` using Visual Studio
-* On top menu, select the build configuration (Debug/Nightly/Release) and click on `Build > Build Solution` to compile
+### System requirements
 
-### Requirements:
+- OS: Windows 10 or later, Windows Server 2016 or later
+- IDE: Visual Studio 2022 or later (VS Code is not supported)
 
-* Windows 10 or later / Windows Server 2016 or later
-* Visual Studio 2022 or later (VS Code is not supported)
-* Open `Visual Studio Installer` menu, click on "Modify" and install these components:
-  * Workload: `Desktop Development with C++`
-  * Individual component: `MSVC v143 - VS 2022 C++ x64/x86 build tools`
+### Set up environment
+
+Open `Visual Studio Installer`, click on `Modify`, and install the following components:
+- Workload: `Desktop Development with C++`
+- Individual component: `MSVC v143 - VS 2022 C++ x64/x86 build tools`
 
 > [!NOTE]
-> The project uses `MSVC v143` from VS 2022 for backward compatibility. When using VS newer than 2022, you can either choose to update the project on `Project > Retarget solution` to use the latest version already installed (e.g., `MSVC v145` from VS 2026), or install the `MSVC v143` individual component and safely ignore the message requesting an update to the latest version
+> The project uses `MSVC v143` platform toolset from VS 2022 for backward compatibility. When opening the project file for the first time using a newer VS, it will request to retarget the project platform toolset to the latest version already installed (e.g., `MSVC v145` from VS 2026). You can either choose to retarget it, or install the `MSVC v143` individual component and safely ignore the request
+
+### Build the project
+
+- Open the project file `SphereSvr.vcxproj` using Visual Studio
+- On the top menu, select the build configuration (`Debug`/`Nightly`/`Release`) and click on `Build > Build Solution`
 
 ## Building on Linux (GCC)
 
 ### Set up environment
 
-<details>
+#### Debian-based (Debian 11 or later, Ubuntu 20.04 or later)
 
-  <summary>Ubuntu 14.04 or later (click to expand)</summary>
+1. Add MySQL 8.0 support to the package repository (only required for Debian 11 or later, Ubuntu 26.04 or later):
+   ```
+   curl -fL https://dev.mysql.com/get/mysql-apt-config_0.8.39-1_all.deb -o mysql-apt-config.deb
+   echo "mysql-apt-config mysql-apt-config/select-server select mysql-8.0" | sudo debconf-set-selections
+   sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config.deb
+   sudo apt-get update
+   rm mysql-apt-config.deb
+   ```
 
-  #### Add MySQL 5.7 support
+2. Install required packages:
+   ```
+   sudo apt-get install git gcc g++ make libmysqlclient-dev=8.0*
+   ```
 
-  * Ubuntu 14.04:
-    * Add MySQL 5.7 support to the package repository
-      ```
-      sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mysql.gpg --keyserver keyserver.ubuntu.com --recv-keys B7B3B788A8D3785C
-      echo "deb [signed-by=/usr/share/keyrings/mysql.gpg] https://repo.mysql.com/apt/ubuntu/ bionic mysql-5.7" | sudo tee /etc/apt/sources.list.d/mysql.list > /dev/null
-      sudo apt-get update
-      ```
-  * Ubuntu 16.04 to 18.04:
-    * > Skip this step (MySQL 5.7 is already supported)
-  * Ubuntu 20.04 or later:
-    * Remove default MySQL package
-      ```
-      sudo apt-get -q remove libmysqlclient-dev
-      ```
-    * Add MySQL 5.7 support to the package repository
-      ```
-      sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mysql.gpg --keyserver keyserver.ubuntu.com --recv-keys B7B3B788A8D3785C
-      echo "deb [signed-by=/usr/share/keyrings/mysql.gpg] https://repo.mysql.com/apt/ubuntu/ bionic mysql-5.7" | sudo tee /etc/apt/sources.list.d/mysql.list > /dev/null
-      sudo apt-get update
-      ```
+#### RHEL-based (RHEL, Rocky Linux, AlmaLinux, Oracle Linux) 8 or later
 
-  #### Install required packages
+1. Enable MySQL 8.0 module stream:
+   ```
+   sudo dnf module enable mysql:8.0
+   ```
 
-  ```
-  sudo apt-get install git gcc g++ make libmysqlclient-dev=5.7*
-  ```
-
-</details>
-
-<details>
-
-  <summary>RHEL 6 / Oracle Linux 6 / CentOS 6 or later (click to expand)</summary>
-
-  #### Add MySQL support
-
-  * Add MySQL support to the package repository
-    * RHEL 6 / Oracle Linux 6 / CentOS 6:
-      ```
-      sudo yum localinstall https://dev.mysql.com/get/mysql80-community-release-el6-11.noarch.rpm
-      ```
-    * RHEL 7 / Oracle Linux 7 / CentOS 7:
-      ```
-      sudo yum localinstall https://dev.mysql.com/get/mysql84-community-release-el7-4.noarch.rpm
-      ```
-    * RHEL 8 / Oracle Linux 8 / CentOS 8:
-      ```
-      sudo yum localinstall https://dev.mysql.com/get/mysql84-community-release-el8-3.noarch.rpm
-      sudo yum module disable mysql
-      ```
-    * RHEL 9 / Oracle Linux 9 / CentOS 9:
-      ```
-      sudo yum localinstall https://dev.mysql.com/get/mysql84-community-release-el9-4.noarch.rpm
-      sudo yum module disable mysql
-      ```
-    * RHEL 10 / Oracle Linux 10 / CentOS 10:
-      ```
-      sudo yum localinstall https://dev.mysql.com/get/mysql84-community-release-el10-3.noarch.rpm
-      sudo yum module disable mysql
-      ```
-
-  * Enable MySQL 5.7 in the package repository
-    ```
-    sudo yum-config-manager --disable mysql-8.4-lts-community
-    sudo yum-config-manager --disable mysql80-community
-    sudo yum-config-manager --enable mysql57-community
-    ```
-
-  #### Install required packages
-
-  ```
-  sudo yum install git gcc-c++ glibc-devel mysql-community-devel
-  ```
-
-</details>
+2. Install required packages:
+   ```
+   sudo dnf install git gcc-c++ glibc-devel mysql-devel
+   ```
 
 ### Get the source code
 
 ```
-git clone https://github.com/SphereServer/Source.git
+git clone https://github.com/Sphereserver/Source.git
 cd Source
 ```
 
-### Compile the source code
+### Build the project
 
-* To compile Debug build:
-  ```
-  make DEBUG=1
-  ```
-* To compile Nightly build:
-  ```
-  make NIGHTLY=1
-  ```
-* To compile Release build:
-  ```
-  make
-  ```
+| Build type | Command          |
+| ---------- | ---------------- |
+| Debug      | `make DEBUG=1`   |
+| Nightly    | `make NIGHTLY=1` |
+| Release    | `make`           |
 
-## Coding notes for new contributors
+## Contributing
 
-* Make sure you can compile and run the program before pushing a commit.
-* Rebasing instead of pulling the project is a better practice to avoid unnecessary "merge branch master" commits.
-* To keep backward compatibility, adding/removing/changing anything that was working in one way for years should be followed by an .ini setting when the changes can't be replicated on scripts.
-* Prefer using C++ named casts (`static_cast<type>(expression)`, `dynamic_cast<type>(expression)`, `const_cast<type>(expression)`, `reinterpret_cast<type>(expression)`) over C-style casts (`(type)expression`) to make sure that invalid casts will throw a compiler error to get fixed at compile-time, preventing the compiler from silently applying an unsafe cast that may throw a runtime error.
+- Make sure you can compile and run the program before pushing a commit.
+- Rebasing instead of pulling the project is a better practice to avoid unnecessary "merge branch master" commits.
+- To keep backward compatibility, adding/removing/changing anything that was working in one way for years should be followed by an .ini setting when the changes can't be replicated on scripts.
+- Prefer using C++ named casts (`static_cast<type>(expression)`, `dynamic_cast<type>(expression)`, `const_cast<type>(expression)`, `reinterpret_cast<type>(expression)`) over C-style casts (`(type)expression`) to ensure invalid casts will cause a compiler error to get fixed at compile time, preventing the compiler from applying an unsafe cast that may cause runtime errors.
 
 ## License
 
--- src/common/CDataBase.cpp
+++ src/common/CDataBase.cpp
@@ -11,8 +11,7 @@ CDataBase::CDataBase()
 
 CDataBase::~CDataBase()
 {
-	if ( m_socket )
-		Close();
+	Close();
 }
 
 void CDataBase::Connect()
@@ -22,43 +21,46 @@ void CDataBase::Connect()
 	if ( m_socket )
 		return;
 
-#ifdef _WIN32
-	if ( mysql_get_client_version() < LIBMYSQL_VERSION_ID )
-		g_Log.EventWarn("Your MySQL client library %s is outdated. For better compatibility, update your 'libmysql.dll' file to version %s\n", mysql_get_client_info(), LIBMYSQL_VERSION);
-#else
-	if ( mysql_get_client_version() < MYSQL_VERSION_ID )
-		g_Log.EventWarn("Your MySQL client library %s is outdated. For better compatibility, update your 'libmysqlclient' (Ubuntu) / 'mysql-community-libs' (CentOS / Red Hat) package to version %s\n", mysql_get_client_info(), MYSQL_SERVER_VERSION);
-#endif
-
 	m_socket = mysql_init(NULL);
 	if ( !m_socket )
 	{
-		g_Log.EventError("Insufficient memory to initialize MySQL client socket\n");
+		g_Log.EventError("Failed to initialize MySQL client (out of memory)\n");
 		return;
 	}
 
-	const char *pszUser = g_Cfg.m_sMySqlUser;
-	const char *pszPassword = g_Cfg.m_sMySqlPass;
-	const char *pszDB = g_Cfg.m_sMySqlDB;
-	const char *pszHost = g_Cfg.m_sMySqlHost;
-	unsigned int uiPort = MYSQL_PORT;
-
-	// If user define server port using hostname:port format, split values into different variables
-	const char *pszArgs = strchr(pszHost, ':');
-	if ( pszArgs )
+	if ( mysql_get_client_version() < LIBMYSQL_VERSION_ID )
 	{
-		char *pszTemp = Str_GetTemp();
-		strncpy(pszTemp, pszHost, HOSTNAME_LENGTH);
-		pszTemp[HOSTNAME_LENGTH - 1] = '\0';
-		*(strchr(pszTemp, ':')) = '\0';
-		uiPort = ATOI(pszArgs + 1);
-		pszHost = pszTemp;
+#ifdef _WIN32
+		const char *pszLibrary = "libmysql.dll file";
+#else
+		const char *pszLibrary = "Debian-based: libmysqlclient21 package / RHEL-based: mysql-libs package";
+#endif
+		g_Log.EventWarn("MySQL client v%s is outdated. For better compatibility, update MySQL client library to v%s (%s)\n", mysql_get_client_info(), LIBMYSQL_VERSION, pszLibrary);
 	}
 
-	if ( mysql_real_connect(m_socket, pszHost, pszUser, pszPassword, pszDB, uiPort, NULL, CLIENT_MULTI_STATEMENTS) )
+	bool fGetServerPublicKey = true;
+	mysql_options(m_socket, MYSQL_OPT_GET_SERVER_PUBLIC_KEY, &fGetServerPublicKey);
+
+	char szHost[CONNECT_STRING_MAXLEN];
+	strncpy(szHost, g_Cfg.m_sMySqlHost, COUNTOF(szHost));
+	szHost[COUNTOF(szHost) - 1] = '\0';
+
+	unsigned int uPort = MYSQL_PORT;
+	char *pszPort = strchr(szHost, ':');
+	if ( pszPort )
+	{
+		*pszPort = '\0';
+		uPort = static_cast<unsigned int>(ATOI(pszPort + 1));
+	}
+
+	const char *pszUser = g_Cfg.m_sMySqlUser;
+	const char *pszPass = g_Cfg.m_sMySqlPass;
+	const char *pszDB = g_Cfg.m_sMySqlDB;
+
+	if ( mysql_real_connect(m_socket, szHost, pszUser, pszPass, pszDB, uPort, NULL, CLIENT_MULTI_STATEMENTS) )
 	{
 		if ( mysql_get_server_version(m_socket) < MYSQL_VERSION_ID )
-			g_Log.EventWarn("Your MySQL server %s is outdated. For better compatibility, update your MySQL server to version %s\n", mysql_get_server_info(m_socket), MYSQL_SERVER_VERSION);
+			g_Log.EventWarn("MySQL server v%s is outdated. For better compatibility, update MySQL server to v%s\n", mysql_get_server_info(m_socket), MYSQL_SERVER_VERSION);
 	}
 	else
 	{
@@ -72,6 +74,9 @@ void CDataBase::Close()
 {
 	ADDTOCALLSTACK("CDataBase::Close");
 	SimpleThreadLock lock(m_connectionMutex);
+	if ( !m_socket )
+		return;
+
 	mysql_close(m_socket);
 	m_socket = NULL;
 }
@@ -89,62 +94,87 @@ bool CDataBase::Query(LPCTSTR pszQuery, CVarDefMap &mapQueryResult)
 	SimpleThreadLock lock(m_connectionMutex);
 
 	int iResultCode = mysql_query(m_socket, pszQuery);
-	if ( iResultCode == 0 )
+	if ( iResultCode != 0 )
 	{
-		MYSQL_RES *pResult = mysql_store_result(m_socket);
-		if ( !pResult )
-			return false;
+		unsigned int uErrNum = mysql_errno(m_socket);
+		g_Log.EventError("MySQL error #%u: %s [Cmd: \"%s\"]\n", uErrNum, mysql_error(m_socket), pszQuery);
 
-		unsigned int uiNumFields = mysql_num_fields(pResult);
-		char key[64];
-		if ( uiNumFields > COUNTOF(key) )
-		{
-			// This check is not really needed, MySQL client should be able to handle the same columns amount of MySQL server (4096).
-			// But since this value is too big and create an 4096 char array at -every- query call is not performance-friendly, maybe
-			// it's better just use an smaller array to prioritize performance.
-			g_Log.EventError("MySQL query returned too many columns [Max: %" FMTSIZE_T " / Cmd: \"%s\"]\n", COUNTOF(key), pszQuery);
-			mysql_free_result(pResult);
-			return false;
-		}
-
-		MYSQL_FIELD *pFields = mysql_fetch_fields(pResult);
-		mapQueryResult.SetNum("NUMROWS", mysql_num_rows(pResult));
-		mapQueryResult.SetNum("NUMCOLS", uiNumFields);
-
-		int iRowNum = 0;
-		char **pRow = NULL;
-		char *pszKey = Str_GetTemp();
-		char *pszVal = NULL;
-		while ( (pRow = mysql_fetch_row(pResult)) != NULL )
-		{
-			for ( unsigned int i = 0; i < uiNumFields; ++i )
-			{
-				pszVal = pRow[i];
-				if ( !iRowNum )
-				{
-					mapQueryResult.SetStr(ITOA(i, key, 10), true, pszVal);
-					mapQueryResult.SetStr(pFields[i].name, true, pszVal);
-				}
-
-				snprintf(pszKey, 24, "%d.%u", iRowNum, i);
-				mapQueryResult.SetStr(pszKey, true, pszVal);
-				snprintf(pszKey, 76, "%d.%s", iRowNum, pFields[i].name);
-				mapQueryResult.SetStr(pszKey, true, pszVal);
-			}
-			++iRowNum;
-		}
-
-		mysql_free_result(pResult);
-		return true;
-	}
-	else
-	{
-		g_Log.EventError("MySQL error #%u: %s [Cmd: \"%s\"]\n", mysql_errno(m_socket), mysql_error(m_socket), pszQuery);
-		if ( (iResultCode == CR_SERVER_GONE_ERROR) || (iResultCode == CR_SERVER_LOST) )
+		if ( (uErrNum == CR_SERVER_GONE_ERROR) || (uErrNum == CR_SERVER_LOST) )
 			Close();
 
 		return false;
 	}
+
+	MYSQL_RES *pResult = mysql_store_result(m_socket);
+	if ( !pResult )
+	{
+		bool fNoFields = (mysql_field_count(m_socket) == 0);
+		while ( mysql_next_result(m_socket) == 0 )
+		{
+			pResult = mysql_store_result(m_socket);
+			if ( pResult )
+				mysql_free_result(pResult);
+		}
+		return fNoFields;
+	}
+
+	unsigned int uNumFields = mysql_num_fields(pResult);
+	unsigned int uMaxFields = 100;
+	if ( uNumFields > uMaxFields )
+	{
+		// MySQL client can handle the same column amount as MySQL server (4096), but it's
+		// better to limit this to prevent key truncation/collision on query result
+		g_Log.EventError("MySQL query exceeded max columns allowed (%u/%u) [Cmd: \"%s\"]\n", uNumFields, uMaxFields, pszQuery);
+
+		mysql_free_result(pResult);
+		while ( mysql_next_result(m_socket) == 0 )
+		{
+			pResult = mysql_store_result(m_socket);
+			if ( pResult )
+				mysql_free_result(pResult);
+		}
+		return false;
+	}
+
+	MYSQL_FIELD *pFields = mysql_fetch_fields(pResult);
+	mapQueryResult.SetNum("NUMROWS", mysql_num_rows(pResult));
+	mapQueryResult.SetNum("NUMCOLS", uNumFields);
+
+	int iRowNum = 0;
+	MYSQL_ROW pRow;
+	char szKey[128];
+	char *pszVal = NULL;
+	while ( (pRow = mysql_fetch_row(pResult)) != NULL )
+	{
+		for ( unsigned int i = 0; i < uNumFields; ++i )
+		{
+			pszVal = pRow[i];
+			if ( iRowNum == 0 )
+			{
+				snprintf(szKey, sizeof(szKey), "%u", i);
+				mapQueryResult.SetStr(szKey, true, pszVal);
+
+				snprintf(szKey, sizeof(szKey), "%s", pFields[i].name);
+				mapQueryResult.SetStr(szKey, true, pszVal);
+			}
+
+			snprintf(szKey, sizeof(szKey), "%d.%u", iRowNum, i);
+			mapQueryResult.SetStr(szKey, true, pszVal);
+
+			snprintf(szKey, sizeof(szKey), "%d.%s", iRowNum, pFields[i].name);
+			mapQueryResult.SetStr(szKey, true, pszVal);
+		}
+		++iRowNum;
+	}
+
+	mysql_free_result(pResult);
+	while ( mysql_next_result(m_socket) == 0 )
+	{
+		pResult = mysql_store_result(m_socket);
+		if ( pResult )
+			mysql_free_result(pResult);
+	}
+	return true;
 }
 
 bool __cdecl CDataBase::Queryf(CVarDefMap &mapQueryResult, char *pFormat, ...)
@@ -154,7 +184,7 @@ bool __cdecl CDataBase::Queryf(CVarDefMap &mapQueryResult, char *pFormat, ...)
 	va_list marker;
 
 	va_start(marker, pFormat);
-	_vsnprintf(pszBuffer, pszBuffer.realLength(), pFormat, marker);
+	vsnprintf(pszBuffer, pszBuffer.realLength(), pFormat, marker);
 	va_end(marker);
 
 	return Query(pszBuffer, mapQueryResult);
@@ -170,24 +200,29 @@ bool CDataBase::Exec(LPCTSTR pszQuery)
 	SimpleThreadLock lock(m_connectionMutex);
 
 	int iResultCode = mysql_query(m_socket, pszQuery);
-	if ( iResultCode == 0 )
+	if ( iResultCode != 0 )
 	{
-		// Result must be retrieved from server even when no data is expected, otherwise the server will think the client has lost connection
-		MYSQL_RES *pResult = mysql_store_result(m_socket);
-		if ( !pResult )
-			return false;
+		unsigned int uErrNum = mysql_errno(m_socket);
+		g_Log.EventError("MySQL error #%u: %s [Cmd: \"%s\"]\n", uErrNum, mysql_error(m_socket), pszQuery);
 
-		mysql_free_result(pResult);
-		return true;
-	}
-	else
-	{
-		g_Log.EventError("MySQL error #%u: %s [Cmd: \"%s\"]\n", mysql_errno(m_socket), mysql_error(m_socket), pszQuery);
-		if ( (iResultCode == CR_SERVER_GONE_ERROR) || (iResultCode == CR_SERVER_LOST) )
+		if ( (uErrNum == CR_SERVER_GONE_ERROR) || (uErrNum == CR_SERVER_LOST) )
 			Close();
 
 		return false;
 	}
+
+	MYSQL_RES *pResult = mysql_store_result(m_socket);
+	if ( pResult )
+		mysql_free_result(pResult);
+
+	while ( mysql_next_result(m_socket) == 0 )
+	{
+		pResult = mysql_store_result(m_socket);
+		if ( pResult )
+			mysql_free_result(pResult);
+	}
+
+	return true;
 }
 
 bool __cdecl CDataBase::Execf(char *pFormat, ...)
@@ -197,7 +232,7 @@ bool __cdecl CDataBase::Execf(char *pFormat, ...)
 	va_list marker;
 
 	va_start(marker, pFormat);
-	_vsnprintf(pszBuffer, pszBuffer.realLength(), pFormat, marker);
+	vsnprintf(pszBuffer, pszBuffer.realLength(), pFormat, marker);
 	va_end(marker);
 
 	return Exec(pszBuffer);
@@ -261,6 +296,21 @@ void CDataBase::OnTick()
 	}
 }
 
+bool CDataBase::r_GetRef(LPCTSTR &pszKey, CScriptObj *&pRef)
+{
+	ADDTOCALLSTACK("CDataBase::r_GetRef");
+	UNREFERENCED_PARAMETER(pszKey);
+	UNREFERENCED_PARAMETER(pRef);
+	return false;
+}
+
+bool CDataBase::r_LoadVal(CScript &s)
+{
+	ADDTOCALLSTACK("CDataBase::r_LoadVal");
+	UNREFERENCED_PARAMETER(s);
+	return false;
+}
+
 enum DBO_TYPE
 {
 	DBO_AEXECUTE,
@@ -280,39 +330,6 @@ const LPCTSTR CDataBase::sm_szLoadKeys[DBO_QTY + 1] =
 	"ROW",
 	NULL
 };
-
-enum DBOV_TYPE
-{
-	DBOV_CLOSE,
-	DBOV_CONNECT,
-	DBOV_EXECUTE,
-	DBOV_QUERY,
-	DBOV_QTY
-};
-
-const LPCTSTR CDataBase::sm_szVerbKeys[DBOV_QTY + 1] =
-{
-	"CLOSE",
-	"CONNECT",
-	"EXECUTE",
-	"QUERY",
-	NULL
-};
-
-bool CDataBase::r_GetRef(LPCTSTR &pszKey, CScriptObj *&pRef)
-{
-	ADDTOCALLSTACK("CDataBase::r_GetRef");
-	UNREFERENCED_PARAMETER(pszKey);
-	UNREFERENCED_PARAMETER(pRef);
-	return false;
-}
-
-bool CDataBase::r_LoadVal(CScript &s)
-{
-	ADDTOCALLSTACK("CDataBase::r_LoadVal");
-	UNREFERENCED_PARAMETER(s);
-	return false;
-}
 
 bool CDataBase::r_WriteVal(LPCTSTR pszKey, CGString &sVal, CTextConsole *pSrc)
 {
@@ -338,7 +355,7 @@ bool CDataBase::r_WriteVal(LPCTSTR pszKey, CGString &sVal, CTextConsole *pSrc)
 				sVal.FormatVal(AsyncQueue((index == DBO_AQUERY), ppArgs[0], ppArgs[1]));
 			else
 			{
-				g_Log.EventError("Invalid %s arguments\n", sm_szLoadKeys[index]);
+				g_Log.EventError("%s: invalid arguments\n", sm_szLoadKeys[index]);
 				sVal = "0";
 			}
 			return true;
@@ -354,12 +371,18 @@ bool CDataBase::r_WriteVal(LPCTSTR pszKey, CGString &sVal, CTextConsole *pSrc)
 			GETNONWHITESPACE(pszKey);
 			sVal = "";
 
-			if ( pszKey[0] != '\0' )
+			if ( m_socket && (pszKey[0] != '\0') )
 			{
-				TCHAR *pszEscapedString = Str_GetTemp();
-				SimpleThreadLock lock(m_connectionMutex);
-				if ( m_socket && mysql_real_escape_string(m_socket, pszEscapedString, pszKey, static_cast<unsigned long>(strlen(pszKey))) )
-					sVal = pszEscapedString;
+				char szEscapedString[THREAD_STRING_LENGTH];
+				size_t iLen = strlen(pszKey);
+				if ( iLen > (sizeof(szEscapedString) / 2) - 1 )
+					g_Log.EventError("%s: args exceeded max length allowed (%zu/%zu)\n", sm_szLoadKeys[index], iLen, (sizeof(szEscapedString) / 2) - 1);
+				else
+				{
+					SimpleThreadLock lock(m_connectionMutex);
+					if ( mysql_real_escape_string(m_socket, szEscapedString, pszKey, static_cast<unsigned long>(iLen)) )
+						sVal = szEscapedString;
+				}
 			}
 			return true;
 		}
@@ -375,6 +398,24 @@ bool CDataBase::r_WriteVal(LPCTSTR pszKey, CGString &sVal, CTextConsole *pSrc)
 	}
 }
 
+enum DBOV_TYPE
+{
+	DBOV_CLOSE,
+	DBOV_CONNECT,
+	DBOV_EXECUTE,
+	DBOV_QUERY,
+	DBOV_QTY
+};
+
+const LPCTSTR CDataBase::sm_szVerbKeys[DBOV_QTY + 1] =
+{
+	"CLOSE",
+	"CONNECT",
+	"EXECUTE",
+	"QUERY",
+	NULL
+};
+
 bool CDataBase::r_Verb(CScript &s, CTextConsole *pSrc)
 {
 	ADDTOCALLSTACK("CDataBase::r_Verb");
@@ -386,27 +427,21 @@ bool CDataBase::r_Verb(CScript &s, CTextConsole *pSrc)
 	switch ( index )
 	{
 		case DBOV_CLOSE:
-		{
-			if ( m_socket )
-				Close();
+			Close();
 			return true;
-		}
+
 		case DBOV_CONNECT:
-		{
-			if ( !m_socket )
-				Connect();
+			Connect();
 			return true;
-		}
+
 		case DBOV_EXECUTE:
-		{
 			Exec(s.GetArgRaw());
 			return true;
-		}
+
 		case DBOV_QUERY:
-		{
 			Query(s.GetArgRaw(), m_QueryResult);
 			return true;
-		}
+
 		default:
 			return false;
 	}
